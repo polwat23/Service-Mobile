dbf6029c2a0be5ab2c45d1cc1a44e93c800ea5fb
dev_gensoft <developer@gensoft.co.th>
2019-10-30 17:49:05 +0700

    Edit pull_repo.bat

--- pull_repo.bat
+++ pull_repo.bat
@@ -1,7 +1,7 @@
 @echo off
 
-cd C:\File Mobile\Demo\Service-Demo
+cd C:\File Mobile\Demo\Service-Demo-Test
 
-"C:\Program Files\Git\bin\git" pull origin demo
+"C:\Program Files\Git\bin\git" pull origin demo-test
 
 pause
