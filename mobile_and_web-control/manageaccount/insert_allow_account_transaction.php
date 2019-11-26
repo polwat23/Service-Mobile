7877259a974c242828eea6b2718853b17d32e620
dev_gensoft <developer@gensoft.co.th>
2019-11-26 22:07:28 +0700

    Add insert account allow and change status

---
+++ mobile_and_web-control/manageaccount/insert_allow_account_transaction.php
@@ -0,0 +1,49 @@
+<?php
+require_once('../autoload.php');
+
+if($lib->checkCompleteArgument(['user_type','member_no','id_token'],$payload) && $lib->checkCompleteArgument(['menu_component','deptaccount_no'],$dataComing)){
+	if($func->check_permission($payload["user_type"],$dataComing["menu_component"],$conmysql,'ManagementAccount')){
+		if($payload["member_no"] == 'dev@mode'){
+			$member_no = $config["MEMBER_NO_DEV_TRANSACTION"];
+		}else if($payload["member_no"] == 'salemode'){
+			$member_no = $config["MEMBER_NO_SALE_TRANSACTION"];
+		}else{
+			$member_no = $payload["member_no"];
+		}
+		$arrBulkAllow = array();
+		foreach($dataComing["deptaccount_no"] as $deptaccount_no){
+			$arrBulkAllow[] = "('".$deptaccount_no."','".$member_no."')";
+		}
+		$insertDeptAllow = $conmysql->prepare("INSERT INTO gcuserallowacctransaction(deptaccount_no,member_no) 
+												VALUES".implode(',',$arrBulkAllow));
+		if($insertDeptAllow->execute()){
+			if(isset($new_token)){
+				$arrayResult['NEW_TOKEN'] = $new_token;
+			}
+			$arrayResult['RESULT'] = TRUE;
+			echo json_encode($arrayResult);
+		}else{
+			$arrayResult['RESPONSE_CODE'] = "WS1024";
+			$arrayResult['RESPONSE_MESSAGE'] = "Cannot insert into user allow account transaction";
+			$arrayResult['RESULT'] = FALSE;
+			http_response_code(403);
+			echo json_encode($arrayResult);
+			exit();
+		}
+	}else{
+		$arrayResult['RESPONSE_CODE'] = "WS0006";
+		$arrayResult['RESPONSE_MESSAGE'] = "Not permission this menu";
+		$arrayResult['RESULT'] = FALSE;
+		http_response_code(403);
+		echo json_encode($arrayResult);
+		exit();
+	}
+}else{
+	$arrayResult['RESPONSE_CODE'] = "WS4004";
+	$arrayResult['RESPONSE_MESSAGE'] = "Not complete argument";
+	$arrayResult['RESULT'] = FALSE;
+	http_response_code(400);
+	echo json_encode($arrayResult);
+	exit();
+}
+?>
---
+++ mobile_and_web-control/manageaccount/update_data_account_been_allow.php
@@ -0,0 +1,40 @@
+<?php
+require_once('../autoload.php');
+
+if($lib->checkCompleteArgument(['user_type','member_no','id_token'],$payload) && $lib->checkCompleteArgument(['menu_component','deptaccount_no','allow_status'],$dataComing)){
+	if($func->check_permission($payload["user_type"],$dataComing["menu_component"],$conmysql,'ManagementAccount')){
+		$updateAccountBeenbind = $conmysql->prepare("UPDATE gcuserallowacctransaction SET is_use = :allow_status WHERE deptaccount_no = :deptaccount_no");
+		if($updateAccountBeenbind->execute([
+			':allow_status' => $dataComing["allow_status"],
+			':deptaccount_no' => $dataComing["deptaccount_no"],
+		])){
+			if(isset($new_token)){
+				$arrayResult['NEW_TOKEN'] = $new_token;
+			}
+			$arrayResult['RESULT'] = TRUE;
+			echo json_encode($arrayResult);
+		}else{
+			$arrayResult = array();
+			$arrayResult['RESPONSE_CODE'] = "WS1025";
+			$arrayResult['RESPONSE_MESSAGE'] = "Cannot update account allow status";
+			$arrayResult['RESULT'] = FALSE;
+			echo json_encode($arrayResult);
+			exit();
+		}
+	}else{
+		$arrayResult['RESPONSE_CODE'] = "WS0006";
+		$arrayResult['RESPONSE_MESSAGE'] = "Not permission this menu";
+		$arrayResult['RESULT'] = FALSE;
+		http_response_code(403);
+		echo json_encode($arrayResult);
+		exit();
+	}
+}else{
+	$arrayResult['RESPONSE_CODE'] = "WS4004";
+	$arrayResult['RESPONSE_MESSAGE'] = "Not complete argument";
+	$arrayResult['RESULT'] = FALSE;
+	http_response_code(400);
+	echo json_encode($arrayResult);
+	exit();
+}
+?>
