ec4eba96f390efbfe31abc2d754d55e2271af44d
dev_gensoft <developer@gensoft.co.th>
2019-12-10 09:14:19 +0700

    Edit hot fix check resign status in login and logged

--- mobile_and_web-control/login/check_logged.php
+++ mobile_and_web-control/login/check_logged.php
@@ -2,56 +2,68 @@
 require_once('../autoload.php');
 
 if($lib->checkCompleteArgument(['unique_id'],$dataComing)){
-	$checkUserlogin = $conmysql->prepare("SELECT id_userlogin,is_login FROM gcuserlogin WHERE id_token = :id_token and is_login <> '0'
-											and member_no = :member_no and unique_id = :unique_id");
-	$checkUserlogin->execute([
-		':id_token' => $payload["id_token"],
-		':member_no' => $payload["member_no"],
-		':unique_id' => $dataComing["unique_id"]
-	]);
-	if($checkUserlogin->rowCount() > 0){
-		$rowLog = $checkUserlogin->fetch();
-		if($rowLog["is_login"] == '1'){
-			try{
-				$logAccess = [
-					"access_date" => date('Y-m-d H:i:s'), 
-					"member_no" => $payload["member_no"], 
-					"access_token" => $access_token,
-					"ip_address" => $dataComing["ip_address"] ?? 'unknown',
-					"id_userlogin" => $rowLog["id_userlogin"]
-				];
-				$conmongo->GCLOGUSERACCESSAFTERLOGIN->insertOne($logAccess);
-			}catch(Exception $e) {
-				$lib->addLogtoTxt([
-					"access_date" => date('Y-m-d H:i:s'), 
-					"member_no" => $payload["member_no"], 
-					"access_token" => $access_token,
-					"ip_address" => $dataComing["ip_address"] ?? 'unknown',
-					"id_userlogin" => $rowLog["id_userlogin"]
-				],'GCLOGUSERACCESSAFTERLOGIN');
+	$checkMemberResign = $conoracle->prepare("SELECT resign_status FROM mbmembmaster WHERE member_no = :member_no");
+	$checkMemberResign->execute([':member_no' => $payload["member_no"]]);
+	$rowMemberResign = $checkMemberResign->fetch();
+	if(isset($rowMemberResign["RESIGN_STATUS"]) && $rowMemberResign["RESIGN_STATUS"] == 0){
+		$checkUserlogin = $conmysql->prepare("SELECT id_userlogin,is_login FROM gcuserlogin WHERE id_token = :id_token and is_login <> '0'
+												and member_no = :member_no and unique_id = :unique_id");
+		$checkUserlogin->execute([
+			':id_token' => $payload["id_token"],
+			':member_no' => $payload["member_no"],
+			':unique_id' => $dataComing["unique_id"]
+		]);
+		if($checkUserlogin->rowCount() > 0){
+			$rowLog = $checkUserlogin->fetch();
+			if($rowLog["is_login"] == '1'){
+				try{
+					$logAccess = [
+						"access_date" => date('Y-m-d H:i:s'), 
+						"member_no" => $payload["member_no"], 
+						"access_token" => $access_token,
+						"ip_address" => $dataComing["ip_address"] ?? 'unknown',
+						"id_userlogin" => $rowLog["id_userlogin"]
+					];
+					$conmongo->GCLOGUSERACCESSAFTERLOGIN->insertOne($logAccess);
+				}catch(Exception $e) {
+					$lib->addLogtoTxt([
+						"access_date" => date('Y-m-d H:i:s'), 
+						"member_no" => $payload["member_no"], 
+						"access_token" => $access_token,
+						"ip_address" => $dataComing["ip_address"] ?? 'unknown',
+						"id_userlogin" => $rowLog["id_userlogin"]
+					],'GCLOGUSERACCESSAFTERLOGIN');
+				}
+				if(isset($new_token)){
+					$arrayResult['NEW_TOKEN'] = $new_token;
+				}
+				$arrayResult['RESULT'] = TRUE;
+			}else{
+				if($rowLog["is_login"] == '-9' || $rowLog["is_login"] == '-10') {
+					$func->revoke_alltoken($payload["id_token"],'-9',true);
+				}else if($rowLog["is_login"] == '-8' || $rowLog["is_login"] == '-99'){
+					$func->revoke_alltoken($payload["id_token"],'-8',true);
+				}else if($rowLog["is_login"] == '-7'){
+					$func->revoke_alltoken($payload["id_token"],'-7',true);
+				}
+				$arrayResult["RESPONSE_MESSAGE"] = $config['LOGOUT'.$rowLog["is_login"]];
+				$arrayResult['RESULT'] = FALSE;
 			}
-			if(isset($new_token)){
-				$arrayResult['NEW_TOKEN'] = $new_token;
-			}
-			$arrayResult['RESULT'] = TRUE;
+			echo json_encode($arrayResult);
 		}else{
-			if($rowLog["is_login"] == '-9' || $rowLog["is_login"] == '-10') {
-				$func->revoke_alltoken($payload["id_token"],'-9',true);
-			}else if($rowLog["is_login"] == '-8' || $rowLog["is_login"] == '-99'){
-				$func->revoke_alltoken($payload["id_token"],'-8',true);
-			}else if($rowLog["is_login"] == '-7'){
-				$func->revoke_alltoken($payload["id_token"],'-7',true);
-			}
-			$arrayResult["RESPONSE_MESSAGE"] = $config['LOGOUT'.$rowLog["is_login"]];
+			$arrayResult['RESPONSE_CODE'] = "WS0009";
+			$arrayResult['RESPONSE_MESSAGE'] = "You cannot access please login";
 			$arrayResult['RESULT'] = FALSE;
+			http_response_code(403);
+			echo json_encode($arrayResult);
+			exit();
 		}
-		echo json_encode($arrayResult);
 	}else{
-		$arrayResult['RESPONSE_CODE'] = "WS0009";
-		$arrayResult['RESPONSE_MESSAGE'] = "You cannot access please login";
+		$arrayResult['RESPONSE_CODE'] = "WS0021";
+		$arrayResult['RESPONSE_MESSAGE'] = "ระบบไม่รองรับสมาชิกเกษียณ";
 		$arrayResult['RESULT'] = FALSE;
-		http_response_code(403);
 		echo json_encode($arrayResult);
+		http_response_code(403);
 		exit();
 	}
 }else{
--- mobile_and_web-control/login/check_login_and_insert_userlog.php
+++ mobile_and_web-control/login/check_login_and_insert_userlog.php
@@ -12,159 +12,12 @@ if($lib->checkCompleteArgument(['member_no','api_token','password','unique_id'],
 		exit();
 	}
 	$member_no = strtolower(str_pad($dataComing["member_no"],8,0,STR_PAD_LEFT));
-	if(strlen($dataComing["password"]) === 13){
-		$checkMember = $conoracle->prepare("SELECT card_person,resign_status,mem_telmobile FROM mbmembmaster WHERE member_no = :member_no and card_person = :password");
-		$checkMember->execute([
-			':member_no' => $member_no,
-			':password' => $dataComing["password"]
-		]);
-		$rowMemberChecked = $checkMember->fetch();
-		if(isset($rowMemberChecked["CARD_PERSON"])){
-			if($rowMemberChecked["RESIGN_STATUS"] == 0){
-				$arrayResult['TEL'] = $rowMemberChecked["MEM_TELMOBILE"];
-				$arrayResult['TEL_FORMAT'] = $lib->formatphone($rowMemberChecked["MEM_TELMOBILE"],' ');
-				$arrayResult['VERIFY'] = TRUE;
-				$arrayResult['RESULT'] = TRUE;
-				echo json_encode($arrayResult);
-				exit();
-			}else{
-				$arrayResult['RESPONSE_CODE'] = "WS0021";
-				$arrayResult['RESPONSE_MESSAGE'] = "ระบบไม่รองรับสมาชิกเกษียณ";
-				$arrayResult['RESULT'] = FALSE;
-				echo json_encode($arrayResult);
-				http_response_code(403);
-				exit();
-			}
-		}else{
-			$checkLogin = $conmysql->prepare("SELECT password,user_type,pin,account_status,temppass FROM gcmemberaccount 
-												WHERE member_no = :member_no and account_status NOT IN('-8','-7','-6')");
-			$checkLogin->execute([':member_no' => $member_no]);
-			if($checkLogin->rowCount() > 0){
-				$rowPassword = $checkLogin->fetch();
-				if($rowPassword['account_status'] == '-9'){
-					if($dataComing["password"] == $rowPassword['temppass']){
-						$valid_pass = true;
-					}else{
-						$valid_pass = false;
-					}
-				}else{
-					$valid_pass = password_verify($dataComing["password"], $rowPassword['password']);
-				}
-				if ($valid_pass) {
-					$refresh_token = $lib->generate_token();
-					$fcm_token = (isset($dataComing["fcm_token"]) && $dataComing["fcm_token"] != '' ? $dataComing["fcm_token"] : null);
-					try{
-						$conmysql->beginTransaction();
-						$updateOldToken = $conmysql->prepare("UPDATE gctoken SET at_is_revoke = '-9',rt_is_revoke = '-9',
-																rt_expire_date = NOW(),at_expire_date = NOW() 
-																WHERE unique_id = :unique_id and (at_is_revoke = '0' OR rt_is_revoke = '0')");
-						$updateOldToken->execute([
-							':unique_id' => $dataComing["unique_id"]
-						]);
-						$insertToken = $conmysql->prepare("INSERT INTO gctoken(refresh_token,unique_id,channel,device_name,ip_address,fcm_token) 
-															VALUES(:refresh_token,:unique_id,:channel,:device_name,:ip_address,:fcm_token)");
-						if($insertToken->execute([
-							':refresh_token' => $refresh_token,
-							':unique_id' => $dataComing["unique_id"],
-							':channel' => $arrPayload["PAYLOAD"]["channel"],
-							':device_name' => $arrPayload["PAYLOAD"]["device_name"],
-							':ip_address' => $arrPayload["PAYLOAD"]["ip_address"],
-							':fcm_token' => $fcm_token ?? null
-						])){
-							$id_token = $conmysql->lastInsertId();
-							if(isset($dataComing["firsttime"])){
-								$firstapp = 0;
-							}else{
-								$firstapp = 1;
-							}
-							$insertLogin = $conmysql->prepare("INSERT INTO gcuserlogin(member_no,device_name,channel,unique_id,status_firstapp,id_token) 
-														VALUES(:member_no,:device_name,:channel,:unique_id,:firstapp,:id_token)");
-							if($insertLogin->execute([
-								':member_no' => $member_no,
-								':device_name' => $arrPayload["PAYLOAD"]["device_name"],
-								':channel' => $arrPayload["PAYLOAD"]["channel"],
-								':unique_id' => $dataComing["unique_id"],
-								':firstapp' => $firstapp,
-								':id_token' => $id_token
-							])){
-								$arrPayloadNew = array();
-								$arrPayloadNew['id_userlogin'] = $conmysql->lastInsertId();
-								$arrPayloadNew['user_type'] = $rowPassword['user_type'];
-								$arrPayloadNew['id_token'] = $id_token;
-								$arrPayloadNew['member_no'] = $member_no;
-								if($arrPayload["PAYLOAD"]["channel"] == 'mobile_app'){
-									$arrPayloadNew['exp'] = time() + 86400;
-								}else {
-									$arrPayloadNew['exp'] = time() + 900;
-								}
-								$arrPayloadNew['refresh_amount'] = 0;
-								$access_token = $jwt_token->customPayload($arrPayloadNew, $config["SECRET_KEY_JWT"]);
-								$updateAccessToken = $conmysql->prepare("UPDATE gctoken SET access_token = :access_token WHERE id_token = :id_token");
-								if($updateAccessToken->execute([
-									':access_token' => $access_token,
-									':id_token' => $id_token
-								])){
-									$conmysql->commit();
-									$arrayResult['REFRESH_TOKEN'] = $refresh_token;
-									$arrayResult['ACCESS_TOKEN'] = $access_token;
-									// Pin Status : 9 => DEV, 1 => TRUE, 0 => FALSE
-									if($arrPayload["PAYLOAD"]["channel"] == 'mobile_app'){
-										if($rowPassword['user_type'] == '9'){
-											$arrayResult['PIN'] = (isset($rowPassword["pin"]) ? 9 : 0);
-										}else{
-											$arrayResult['PIN'] = (isset($rowPassword["pin"]) ? 1 : 0);
-										}
-									}
-									$arrayResult['RESULT'] = TRUE;
-									echo json_encode($arrayResult);
-								}else{
-									$conmysql->rollback();
-									$arrayResult['RESPONSE_CODE'] = "WS0002";
-									$arrayResult['RESPONSE_MESSAGE'] = "Cannot update Access Token";
-									$arrayResult['RESULT'] = FALSE;
-									echo json_encode($arrayResult);
-									exit();
-								}
-							}else{
-								$conmysql->rollback();
-								$arrayResult['RESPONSE_CODE'] = "WS0002";
-								$arrayResult['RESPONSE_MESSAGE'] = "Error! Cannot Insert User Log";
-								$arrayResult['RESULT'] = FALSE;
-								echo json_encode($arrayResult);
-								exit();
-							}
-						}else{
-							$conmysql->rollback();
-							$arrayResult['RESPONSE_CODE'] = "WS0003";
-							$arrayResult['RESPONSE_MESSAGE'] = "Error! Cannot Insert Token";
-							$arrayResult['RESULT'] = FALSE;
-							echo json_encode($arrayResult);
-							exit();
-						}
-					}catch (PDOExecption $e) {
-						$conmysql->rollback();
-						$arrayResult['RESPONSE_CODE'] = "WS9999";
-						$arrayResult['RESPONSE_MESSAGE'] = $e->getMessage();
-						$arrayResult['RESULT'] = FALSE;
-						echo json_encode($arrayResult);
-						exit();
-					}
-				}else{
-					$arrayResult['RESPONSE_CODE'] = "WS0004";
-					$arrayResult['RESPONSE_MESSAGE'] = "Invalid password";
-					$arrayResult['RESULT'] = FALSE;
-					echo json_encode($arrayResult);
-					exit();
-				}
-			}else{
-				$arrayResult['RESPONSE_CODE'] = "WS0005";
-				$arrayResult['RESPONSE_MESSAGE'] = "Not found membership";
-				$arrayResult['RESULT'] = FALSE;
-				echo json_encode($arrayResult);
-				exit();
-			}
-		}
-	}else{
+	$checkMemberResign = $conoracle->prepare("SELECT resign_status FROM mbmembmaster WHERE member_no = :member_no");
+	$checkMemberResign->execute([
+		':member_no' => $member_no
+	]);
+	$rowMemberResign = $checkMemberResign->fetch();
+	if(isset($rowMemberResign["RESIGN_STATUS"]) && $rowMemberResign["RESIGN_STATUS"] == 0){
 		$checkLogin = $conmysql->prepare("SELECT password,user_type,pin,account_status,temppass FROM gcmemberaccount 
 											WHERE member_no = :member_no and account_status NOT IN('-8','-7','-6')");
 		$checkLogin->execute([':member_no' => $member_no]);
@@ -181,6 +34,7 @@ if($lib->checkCompleteArgument(['member_no','api_token','password','unique_id'],
 			}
 			if ($valid_pass) {
 				$refresh_token = $lib->generate_token();
+				$fcm_token = (isset($dataComing["fcm_token"]) && $dataComing["fcm_token"] != '' ? $dataComing["fcm_token"] : null);
 				try{
 					$conmysql->beginTransaction();
 					$updateOldToken = $conmysql->prepare("UPDATE gctoken SET at_is_revoke = '-9',rt_is_revoke = '-9',
@@ -197,7 +51,7 @@ if($lib->checkCompleteArgument(['member_no','api_token','password','unique_id'],
 						':channel' => $arrPayload["PAYLOAD"]["channel"],
 						':device_name' => $arrPayload["PAYLOAD"]["device_name"],
 						':ip_address' => $arrPayload["PAYLOAD"]["ip_address"],
-						':fcm_token' => $dataComing["fcm_token"] ?? null
+						':fcm_token' => $fcm_token ?? null
 					])){
 						$id_token = $conmysql->lastInsertId();
 						if(isset($dataComing["firsttime"])){
@@ -278,11 +132,26 @@ if($lib->checkCompleteArgument(['member_no','api_token','password','unique_id'],
 					exit();
 				}
 			}else{
-				$arrayResult['RESPONSE_CODE'] = "WS0004";
-				$arrayResult['RESPONSE_MESSAGE'] = "Invalid password";
-				$arrayResult['RESULT'] = FALSE;
-				echo json_encode($arrayResult);
-				exit();
+				$checkMember = $conoracle->prepare("SELECT card_person,mem_telmobile FROM mbmembmaster WHERE member_no = :member_no and card_person = :password");
+				$checkMember->execute([
+					':member_no' => $member_no,
+					':password' => $dataComing["password"]
+				]);
+				$rowMemberChecked = $checkMember->fetch();
+				if(isset($rowMemberChecked["CARD_PERSON"])){
+					$arrayResult['TEL'] = $rowMemberChecked["MEM_TELMOBILE"];
+					$arrayResult['TEL_FORMAT'] = $lib->formatphone($rowMemberChecked["MEM_TELMOBILE"],' ');
+					$arrayResult['VERIFY'] = TRUE;
+					$arrayResult['RESULT'] = TRUE;
+					echo json_encode($arrayResult);
+					exit();
+				}else{
+					$arrayResult['RESPONSE_CODE'] = "WS0004";
+					$arrayResult['RESPONSE_MESSAGE'] = "Invalid password";
+					$arrayResult['RESULT'] = FALSE;
+					echo json_encode($arrayResult);
+					exit();
+				}
 			}
 		}else{
 			$arrayResult['RESPONSE_CODE'] = "WS0005";
@@ -291,6 +160,13 @@ if($lib->checkCompleteArgument(['member_no','api_token','password','unique_id'],
 			echo json_encode($arrayResult);
 			exit();
 		}
+	}else{
+		$arrayResult['RESPONSE_CODE'] = "WS0021";
+		$arrayResult['RESPONSE_MESSAGE'] = "ระบบไม่รองรับสมาชิกเกษียณ";
+		$arrayResult['RESULT'] = FALSE;
+		echo json_encode($arrayResult);
+		http_response_code(403);
+		exit();
 	}
 }else{
 	$arrayResult['RESPONSE_CODE'] = "WS4004";
