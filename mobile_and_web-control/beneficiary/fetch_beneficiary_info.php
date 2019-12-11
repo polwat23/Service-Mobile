94ebcb5d53a9f03f038b378c19c427bf68547b47
dev_gensoft <developer@gensoft.co.th>
2019-12-11 17:43:44 +0700

    Edit Beneficiary

--- mobile_and_web-control/beneficiary/fetch_beneficiary_info.php
+++ mobile_and_web-control/beneficiary/fetch_beneficiary_info.php
@@ -10,14 +10,21 @@ if($lib->checkCompleteArgument(['menu_component'],$dataComing)){
 		}else{
 			$member_no = $payload["member_no"];
 		}
-		$getBeneficiary = $conoracle->prepare("select base64_img from fomimagemaster 
+		$getBeneficiary = $conoracle->prepare("select base64_img,data_type from fomimagemaster 
 												where system_code = 'mbshr' and column_name = 'member_no' 
 												and column_data = :member_no and img_type_code = '003' and seq_no = 1");
 		$getBeneficiary->execute([':member_no' => $member_no]);
 		$rowBenefit = $getBeneficiary->fetch();
-		$rowBase64PDF = isset($rowBenefit["BASE64_IMG"]) ? "data:application/pdf;base64,".base64_encode(stream_get_contents($rowBenefit["BASE64_IMG"])) : null;
-		if((isset($rowBase64PDF) && $rowBase64PDF != '') || isset($new_token)){
-			$arrayResult['BENEFICIARY'] = $rowBase64PDF;
+		if($rowBenefit["DATA_TYPE"] == 'pdf'){
+			$rowBase64 = isset($rowBenefit["BASE64_IMG"]) ? "data:application/pdf;base64,".base64_encode(stream_get_contents($rowBenefit["BASE64_IMG"])) : null;
+		}else if($rowBenefit["DATA_TYPE"] == 'png' || $rowBenefit["DATA_TYPE"] == 'jpg' || $rowBenefit["DATA_TYPE"] == 'jpeg'){
+			$rowBase64 = isset($rowBenefit["BASE64_IMG"]) ? "data:image/".$rowBenefit["DATA_TYPE"].";base64,".base64_encode(stream_get_contents($rowBenefit["BASE64_IMG"])) : null;
+		}else{
+			$rowBase64 = isset($rowBenefit["BASE64_IMG"]) ? "data:application/pdf;base64,".base64_encode(stream_get_contents($rowBenefit["BASE64_IMG"])) : null;
+		}
+		if((isset($rowBase64) && $rowBase64 != '') || isset($new_token)){
+			$arrayResult['DATA_TYPE'] = $rowBenefit["DATA_TYPE"] ?? 'pdf';
+			$arrayResult['BENEFICIARY'] = $rowBase64;
 			if(isset($new_token)){
 				$arrayResult['NEW_TOKEN'] = $new_token;
 			}
