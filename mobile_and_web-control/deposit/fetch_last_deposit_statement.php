e6033e177a7f314dfc4e505cf3eb4a3622d82d0b
dev_gensoft <developer@gensoft.co.th>
2019-12-30 15:24:36 +0700

    Edit Last stm deposit & loan

---
+++ mobile_and_web-control/deposit/fetch_last_deposit_statement.php
@@ -0,0 +1,107 @@
+<?php
+require_once('../autoload.php');
+
+if($lib->checkCompleteArgument(['menu_component'],$dataComing)){
+	if($func->check_permission($payload["user_type"],$dataComing["menu_component"],'DepositStatement')){
+		if($payload["member_no"] == 'dev@mode'){
+			$member_no = $config["MEMBER_NO_DEV_DEPOSIT"];
+		}else if($payload["member_no"] == 'salemode'){
+			$member_no = $config["MEMBER_NO_SALE_DEPOSIT"];
+		}else{
+			$member_no = $payload["member_no"];
+		}
+		$arrayResult = array();
+		$arrGroupAccount = array();
+		$arrayGroupSTM = array();
+		$limit = $func->getConstant('limit_stmdeposit');
+		$arrayResult['LIMIT_DURATION'] = $limit;
+		$date_before = date('Y-m-d',strtotime('-'.$limit.' months'));
+		$date_now = date('Y-m-d');
+		$fetchLastStmAcc = $conoracle->prepare("SELECT deptaccount_no from dpdeptmaster where member_no = :member_no and 
+												lastmovement_date = (SELECT MAX(dpm.lastmovement_date) FROM dpdeptmaster dpm WHERE dpm.member_no = :member_no) and deptclose_status <> 1");
+		$fetchLastStmAcc->execute([':member_no' => $member_no]);
+		$rowAccountLastSTM = $fetchLastStmAcc->fetch();
+		$account_no = preg_replace('/-/','',$rowAccountLastSTM["DEPTACCOUNT_NO"]);
+		$getAccount = $conoracle->prepare("SELECT dt.depttype_desc,dp.deptaccount_name,dp.prncbal as BALANCE,
+											(SELECT max(OPERATE_DATE) FROM dpdeptstatement WHERE deptaccount_no = :account_no) as LAST_OPERATE_DATE
+											FROM dpdeptmaster dp LEFT JOIN DPDEPTTYPE dt ON dp.depttype_code = dt.depttype_code and dp.membcat_code = dt.membcat_code
+											WHERE dp.member_no = :member_no and dp.deptclose_status <> 1 and dp.deptaccount_no = :account_no");
+		$getAccount->execute([
+			':member_no' => $member_no,
+			':account_no' => $account_no
+		]);
+		$rowAccount = $getAccount->fetch();
+		$arrAccount = array();
+		$account_no_format = $lib->formataccount($account_no,$func->getConstant('dep_format'));
+		$arrAccount["DEPTACCOUNT_NO"] = $account_no_format;
+		$arrAccount["DEPTACCOUNT_NO_HIDDEN"] = $lib->formataccount_hidden($account_no,$func->getConstant('hidden_dep'));
+		$arrAccount["DEPTACCOUNT_NAME"] = preg_replace('/\"/','',$rowAccount["DEPTACCOUNT_NAME"]);
+		$arrAccount["BALANCE"] = number_format($rowAccount["BALANCE"],2);
+		$arrAccount["LAST_OPERATE_DATE"] = $lib->convertdate($rowAccount["LAST_OPERATE_DATE"],'y-n-d');
+		$arrAccount["LAST_OPERATE_DATE_FORMAT"] = $lib->convertdate($rowAccount["LAST_OPERATE_DATE"],'D m Y');
+		$getStatement = $conoracle->prepare("SELECT dit.DEPTITEMTYPE_DESC AS TYPE_TRAN,dit.SIGN_FLAG,dsm.seq_no,
+											dsm.operate_date,dsm.DEPTITEM_AMT as TRAN_AMOUNT
+											FROM dpdeptstatement dsm LEFT JOIN DPUCFDEPTITEMTYPE dit
+											ON dsm.DEPTITEMTYPE_CODE = dit.DEPTITEMTYPE_CODE 
+											WHERE dsm.deptaccount_no = :account_no and dsm.OPERATE_DATE
+											BETWEEN to_date(:datebefore,'YYYY-MM-DD') and to_date(:datenow,'YYYY-MM-DD') ORDER BY dsm.SEQ_NO DESC");
+		$getStatement->execute([
+			':account_no' => $account_no,
+			':datebefore' => $date_before,
+			':datenow' => $date_now
+		]);
+		while($rowStm = $getStatement->fetch()){
+			$getMemoDP = $conmysql->prepare("SELECT memo_text,memo_icon_path FROM gcmemodept 
+											WHERE deptaccount_no = :account_no and seq_no = :seq_no");
+			$getMemoDP->execute([
+				':account_no' => $account_no,
+				':seq_no' => $rowStm["SEQ_NO"]
+			]);
+			$rowMemo = $getMemoDP->fetch();
+			$arrSTM = array();
+			$arrSTM["TYPE_TRAN"] = $rowStm["TYPE_TRAN"];
+			$arrSTM["SIGN_FLAG"] = $rowStm["SIGN_FLAG"];
+			$arrSTM["SEQ_NO"] = $rowStm["SEQ_NO"];
+			$arrSTM["OPERATE_DATE"] = $lib->convertdate($rowStm["OPERATE_DATE"],'D m Y');
+			$arrSTM["TRAN_AMOUNT"] = number_format($rowStm["TRAN_AMOUNT"],2);
+			$arrSTM["MEMO_TEXT"] = $rowMemo["memo_text"];
+			$arrSTM["MEMO_ICON_PATH"] = $rowMemo["memo_icon_path"];
+			$arrayGroupSTM[] = $arrSTM;
+		}
+		if(sizeof($arrayGroupSTM) > 0 || isset($new_token)){
+			$arrayResult["HEADER"] = $arrAccount;
+			$arrayResult["STATEMENT"] = $arrayGroupSTM;
+			if(isset($new_token)){
+				$arrayResult['NEW_TOKEN'] = $new_token;
+			}
+			$arrayResult["RESULT"] = TRUE;
+			echo json_encode($arrayResult);
+		}else{
+			http_response_code(204);
+			exit();
+		}
+	}else{
+		$arrayResult['RESPONSE_CODE'] = "WS0006";
+		if($lang_locale == 'th'){
+			$arrayResult['RESPONSE_MESSAGE'] = "ท่านไม่มีสิทธิ์ใช้งานเมนูนี้";
+		}else{
+			$arrayResult['RESPONSE_MESSAGE'] = "You not have permission for this menu";
+		}
+		$arrayResult['RESULT'] = FALSE;
+		http_response_code(403);
+		echo json_encode($arrayResult);
+		exit();
+	}
+}else{
+	$arrayResult['RESPONSE_CODE'] = "WS4004";
+	if($lang_locale == 'th'){
+		$arrayResult['RESPONSE_MESSAGE'] = "มีบางอย่างผิดพลาดกรุณาติดต่อสหกรณ์ #WS4004";
+	}else{
+		$arrayResult['RESPONSE_MESSAGE'] = "Something wrong please contact cooperative #WS4004";
+	}
+	$arrayResult['RESULT'] = FALSE;
+	http_response_code(400);
+	echo json_encode($arrayResult);
+	exit();
+}
+?>
---
+++ mobile_and_web-control/loan/fetch_last_loan_statement.php
@@ -0,0 +1,103 @@
+<?php
+require_once('../autoload.php');
+
+if($lib->checkCompleteArgument(['menu_component'],$dataComing)){
+	if($func->check_permission($payload["user_type"],$dataComing["menu_component"],'LoanStatement')){
+		if($payload["member_no"] == 'dev@mode'){
+			$member_no = $config["MEMBER_NO_DEV_LOAN"];
+		}else if($payload["member_no"] == 'salemode'){
+			$member_no = $config["MEMBER_NO_DEV_LOAN"];
+		}else{
+			$member_no = $payload["member_no"];
+		}
+		$arrayResult = array();
+		$arrGroupAccount = array();
+		$arrayGroupSTM = array();
+		$limit = $func->getConstant('limit_stmloan');
+		$arrayResult['LIMIT_DURATION'] = $limit;
+		$date_before = date('Y-m-d',strtotime('-'.$limit.' months'));
+		$date_now = date('Y-m-d');
+		$fetchLastStmAcc = $conoracle->prepare("SELECT loancontract_no from lncontmaster where member_no = :member_no and 
+												lastpayment_date = (SELECT MAX(lnm.lastpayment_date) FROM lncontmaster lnm WHERE lnm.member_no = :member_no) and contract_status = 1");
+		$fetchLastStmAcc->execute([':member_no' => $member_no]);
+		$rowLoanLastSTM = $fetchLastStmAcc->fetch();
+		$contract_no = preg_replace('/\//','',$rowLoanLastSTM["LOANCONTRACT_NO"]);
+		$getContract = $conoracle->prepare("SELECT lt.LOANTYPE_DESC AS LOAN_TYPE,ln.principal_balance as LOAN_BALANCE,
+											ln.loanapprove_amt as APPROVE_AMT,ln.startcont_date,ln.period_payment,period_payamt as PERIOD,
+											LAST_PERIODPAY as LAST_PERIOD,
+											(SELECT max(operate_date) FROM lncontstatement WHERE loancontract_no = :contract_no) as LAST_OPERATE_DATE
+											FROM lncontmaster ln LEFT JOIN LNLOANTYPE lt ON ln.LOANTYPE_CODE = lt.LOANTYPE_CODE 
+											WHERE ln.member_no = :member_no and ln.contract_status = 1 and ln.loancontract_no = :contract_no");
+		$getContract->execute([
+			':member_no' => $member_no,
+			':contract_no' => $contract_no
+		]);
+		$rowContract = $getContract->fetch();
+		$arrContract = array();
+		$arrContract["CONTRACT_NO"] = $lib->formatcontract($contract_no,$func->getConstant('loan_format'));
+		$arrContract["LOAN_BALANCE"] = number_format($rowContract["LOAN_BALANCE"],2);
+		$arrContract["APPROVE_AMT"] = number_format($rowContract["APPROVE_AMT"],2);
+		$arrContract["LAST_OPERATE_DATE"] = $lib->convertdate($rowContract["LAST_OPERATE_DATE"],'y-n-d');
+		$arrContract["LAST_OPERATE_DATE_FORMAT"] = $lib->convertdate($rowContract["LAST_OPERATE_DATE"],'D m Y');
+		$arrContract["STARTCONT_DATE"] = $lib->convertdate($rowContract["STARTCONT_DATE"],'D m Y');
+		$arrContract["PERIOD_PAYMENT"] = number_format($rowContract["PERIOD_PAYMENT"],2);
+		$arrContract["PERIOD"] = $rowContract["LAST_PERIOD"].' / '.$rowContract["PERIOD"];
+		$getStatement = $conoracle->prepare("SELECT lit.LOANITEMTYPE_DESC AS TYPE_DESC,lsm.operate_date,lsm.principal_payment as PRN_PAYMENT,
+											lsm.interest_payment as INT_PAYMENT,sl.payinslip_no
+											FROM lncontstatement lsm LEFT JOIN LNUCFLOANITEMTYPE lit
+											ON lsm.LOANITEMTYPE_CODE = lit.LOANITEMTYPE_CODE 
+											LEFT JOIN slslippayindet sl ON lsm.loancontract_no = sl.loancontract_no and lsm.period = sl.period
+											WHERE lsm.loancontract_no = :contract_no and lsm.operate_date
+											BETWEEN to_date(:datebefore,'YYYY-MM-DD') and to_date(:datenow,'YYYY-MM-DD') ORDER BY lsm.SEQ_NO DESC");
+		$getStatement->execute([
+			':contract_no' => $contract_no,
+			':datebefore' => $date_before,
+			':datenow' => $date_now
+		]);
+		while($rowStm = $getStatement->fetch()){
+			$arrSTM = array();
+			$arrSTM["TYPE_DESC"] = $rowStm["TYPE_DESC"];
+			$arrSTM["SLIP_NO"] = $rowStm["PAYINSLIP_NO"];
+			$arrSTM["OPERATE_DATE"] = $lib->convertdate($rowStm["OPERATE_DATE"],'D m Y');
+			$arrSTM["PRN_PAYMENT"] = number_format($rowStm["PRN_PAYMENT"],2);
+			$arrSTM["INT_PAYMENT"] = number_format($rowStm["INT_PAYMENT"],2);
+			$arrSTM["SUM_PAYMENT"] = number_format($rowStm["INT_PAYMENT"] + $rowStm["PRN_PAYMENT"],2);
+			$arrayGroupSTM[] = $arrSTM;
+		}
+		if(sizeof($arrayGroupSTM) > 0 || isset($new_token)){
+			$arrayResult["HEADER"] = $arrContract;
+			$arrayResult["STATEMENT"] = $arrayGroupSTM;
+			if(isset($new_token)){
+				$arrayResult['NEW_TOKEN'] = $new_token;
+			}
+			$arrayResult["RESULT"] = TRUE;
+			echo json_encode($arrayResult);
+		}else{
+			http_response_code(204);
+			exit();
+		}
+	}else{
+		$arrayResult['RESPONSE_CODE'] = "WS0006";
+		if($lang_locale == 'th'){
+			$arrayResult['RESPONSE_MESSAGE'] = "ท่านไม่มีสิทธิ์ใช้งานเมนูนี้";
+		}else{
+			$arrayResult['RESPONSE_MESSAGE'] = "You not have permission for this menu";
+		}
+		$arrayResult['RESULT'] = FALSE;
+		http_response_code(403);
+		echo json_encode($arrayResult);
+		exit();
+	}
+}else{
+	$arrayResult['RESPONSE_CODE'] = "WS4004";
+	if($lang_locale == 'th'){
+		$arrayResult['RESPONSE_MESSAGE'] = "มีบางอย่างผิดพลาดกรุณาติดต่อสหกรณ์ #WS4004";
+	}else{
+		$arrayResult['RESPONSE_MESSAGE'] = "Something wrong please contact cooperative #WS4004";
+	}
+	$arrayResult['RESULT'] = FALSE;
+	http_response_code(400);
+	echo json_encode($arrayResult);
+	exit();
+}
+?>
