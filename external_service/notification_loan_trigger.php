a6b2cec16b8d8dd35f509653ce83b6ef8e47835d
dev_gensoft <developer@gensoft.co.th>
2020-04-02 11:04:33 +0700

    Edit Sync notify flag

--- external_service/notification_loan_trigger.php
+++ external_service/notification_loan_trigger.php
@@ -9,7 +9,7 @@ use Component\functions;
 $lib = new library();
 $func = new functions();
 
-$fetchDataSTM = $conoracle->prepare("SELECT lut.loanitemtype_desc,lcn.loancontract_no,lcn.entry_date,lcm.member_no,lcn.seq_no,
+$fetchDataSTM = $conoracle->prepare("SELECT lut.loanitemtype_desc,lcn.loancontract_no,lcn.OPERATE_DATE,lcm.member_no,lcn.seq_no,
 									lcn.principal_payment,lcn.interest_payment,lcn.principal_balance
 									from lncontstatement lcn LEFT JOIN lncontmaster lcm ON lcn.loancontract_no = lcm.loancontract_no
 									LEFT JOIN lnucfloanitemtype lut ON lcn.loanitemtype_code = lut.loanitemtype_code
@@ -35,8 +35,8 @@ while($rowSTM = $fetchDataSTM->fetch(PDO::FETCH_ASSOC)){
 		$dataMerge["INTEREST_PAYMENT"] = number_format($rowSTM["INTEREST_PAYMENT"],2);
 		$dataMerge["PRINCIPAL_BALANCE"] = number_format($rowSTM["PRINCIPAL_BALANCE"],2);
 		$dataMerge["ITEMTYPE_DESC"] = $rowSTM["LOANITEMTYPE_DESC"];
-		$dataMerge["DATETIME"] = isset($rowSTM["ENTRY_DATE"]) && $rowSTM["ENTRY_DATE"] != '' ? 
-		$lib->convertdate($rowSTM["ENTRY_DATE"],'D m Y',true) : $lib->convertdate(date('Y-m-d H:i:s'),'D m Y',true);
+		$dataMerge["DATETIME"] = isset($rowSTM["OPERATE_DATE"]) && $rowSTM["OPERATE_DATE"] != '' ? 
+		$lib->convertdate($rowSTM["OPERATE_DATE"],'D m Y',true) : $lib->convertdate(date('Y-m-d H:i:s'),'D m Y',true);
 		$message_endpoint = $lib->mergeTemplate($templateMessage["SUBJECT"],$templateMessage["BODY"],$dataMerge);
 		$arrPayloadNotify["TO"] = array($dest["TOKEN"]);
 		$arrPayloadNotify["MEMBER_NO"] = array($dest["MEMBER_NO"]);
--- external_service/test.bat
+++ external_service/test.bat
@@ -1,5 +1,5 @@
 
 
-C:\Mobile\php7.4\php.exe C:\Mobile\Service-MHD-Test\external_service\notification_guarantee_trigger.php
+C:\Mobile\php7.4\php.exe C:\Mobile\Service-MHD-Test\external_service\notification_share_trigger.php
 
 pause
