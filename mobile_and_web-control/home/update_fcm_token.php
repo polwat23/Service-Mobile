057ade5e91a1f4ef45d9f3eae5fa0d244e6eef99
dev_gensoft <developer@gensoft.co.th>
2020-04-14 09:17:36 +0700

    Add Update FCM Token

---
+++ mobile_and_web-control/home/update_fcm_token.php
@@ -0,0 +1,18 @@
+<?php
+require_once('../autoload.php');
+
+if($lib->checkCompleteArgument(['fcm_token'],$dataComing)){
+	$updateNewToken = $conmysql->prepare("UPDATE gcmemberaccount SET fcm_token = :fcm_token WHERE member_no = :member_no");
+	$updateNewToken->execute([
+		':fcm_token' => $dataComing["fcm_token"],
+		':member_no' => $payload["member_no"]
+	]);
+}else{
+	$arrayResult['RESPONSE_CODE'] = "WS4004";
+	$arrayResult['RESPONSE_MESSAGE'] = $configError[$arrayResult['RESPONSE_CODE']][0][$lang_locale];
+	$arrayResult['RESULT'] = FALSE;
+	http_response_code(400);
+	echo json_encode($arrayResult);
+	exit();
+}
+?>
