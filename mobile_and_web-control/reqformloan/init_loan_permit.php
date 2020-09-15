1282e6580bd91e5e38c14cc44a94f3c5edfd6a88
dev_gensoft <developer@gensoft.co.th>
2020-09-15 10:51:57 +0700

    Edit calculate credit

--- mobile_and_web-control/reqformloan/init_loan_permit.php
+++ mobile_and_web-control/reqformloan/init_loan_permit.php
@@ -14,12 +14,8 @@ if($lib->checkCompleteArgument(['menu_component','loantype_code'],$dataComing)){
 			$maxloan_amt = 0;
 			$oldBal = 0;
 			$loanRequest = TRUE;
-			if($dataComing["loantype_code"] == '10'){
-				include(__DIR__.'/../credit/calculate_loan_emer.php');
-			}else if($dataComing["loantype_code"] == '20'){
-				include(__DIR__.'/../credit/calculate_loan_normal_share_coll.php');
-			}else if($dataComing["loantype_code"] == '30'){
-				include(__DIR__.'/../credit/calculate_loan_special_share_coll.php');
+			if(file_exists(__DIR__.'/../credit/calculate_loan_'.$dataComing["loantype_code"].'.php')){
+				include(__DIR__.'/../credit/calculate_loan_'.$dataComing["loantype_code"].'.php');
 			}else{
 				include(__DIR__.'/../credit/calculate_loan_etc.php');
 			}
