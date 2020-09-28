1504eb185becffa306a41f8623b5d550b9ddcca4
dev_gensoft <developer@gensoft.co.th>
2020-09-28 17:14:09 +0700

    Edit temppass is md5

--- core-control/mobile_admin/manageuser/manageuseraccount/update_password_account.php
+++ core-control/mobile_admin/manageuser/manageuseraccount/update_password_account.php
@@ -13,7 +13,7 @@ if($lib->checkCompleteArgument(['unique_id','member_no'],$dataComing)){
 		]);
 		$rowcitizenid = $fetchCitizenID->fetch(PDO::FETCH_ASSOC);
 		$new_password = $rowcitizenid["CARD_PERSON"];
-		$repassword = $conmysql->prepare("UPDATE gcmemberaccount SET prev_acc_status = account_status,temppass = :newpassword,account_status = '-9',counter_wrongpass = 0 WHERE member_no = :member_no");
+		$repassword = $conmysql->prepare("UPDATE gcmemberaccount SET prev_acc_status = account_status,temppass = :newpassword,account_status = '-9',counter_wrongpass = 0,temppass_is_md5 = '0' WHERE member_no = :member_no");
 		if($repassword->execute([
 				':newpassword' => password_hash($new_password,PASSWORD_DEFAULT),
 				':member_no' => $dataComing["member_no"]
