dbfcec688521d0f3047a4009ce8ff4445a64ccc4
dev_gensoft <developer@gensoft.co.th>
2020-09-29 11:26:43 +0700

    Stable deploy

--- external_service/backupDB.bat
+++ external_service/backupDB.bat
@@ -1,22 +1,22 @@
 @echo off
 
 
-del C:\Mobile\service-srn\external_service\backupDB_srn.zip
+del C:\Mobile\service-nkt\external_service\backupDB_nkt.zip
 
 cd C:\Program Files\MariaDB 10.4\bin
 
 C:
 
-mysqldump.exe -hlocalhost -P3306 -u root -p@SRN2020 mobile_srn > C:\Mobile\service-srn\external_service\backupDB_srn.sql
+mysqldump.exe -hlocalhost -P3306 -u root -p@NKT2020 mobile_nkt > C:\Mobile\service-nkt\external_service\backupDB_nkt.sql
 
-"C:\Program Files\7-Zip\7z.exe" a -r C:\Mobile\service-srn\external_service\backupDB_srn.zip C:\Mobile\service-srn\external_service\backupDB_srn.sql
+"C:\Program Files\7-Zip\7z.exe" a -r C:\Mobile\service-nkt\external_service\backupDB_nkt.zip C:\Mobile\service-nkt\external_service\backupDB_nkt.sql
 
-del C:\Mobile\service-srn\external_service\backupDB_srn.sql
+del C:\Mobile\service-nkt\external_service\backupDB_nkt.sql
 
 cd C:\Program Files (x86)\WinSCP
 
 C:
 
-winscp.exe /command "open ftp://ftp_backup:@Gensoft2018@203.154.140.14/incoming" "put C:\Mobile\service-srn\external_service\backupDB_srn.zip" "exit"
+winscp.exe /command "open ftp://ftp_backup:@Gensoft2018@203.154.140.14/incoming" "put C:\Mobile\service-nkt\external_service\backupDB_nkt.zip" "exit"
 
-del C:\Mobile\service-srn\external_service\backupDB_srn.zip
+del C:\Mobile\service-nkt\external_service\backupDB_nkt.zip
--- external_service/notify_share.bat
+++ external_service/notify_share.bat
@@ -1,3 +1,3 @@
 @echo off
 
-C:\Mobile\php7.4\php.exe C:\Mobile\service-srn\external_service\notification_share_trigger.php
+C:\Mobile\php7.4\php.exe C:\Mobile\service-nkt\external_service\notification_share_trigger.php
