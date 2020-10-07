19566b4452c75d0cdacb23db748f16104da0fd9a
dev_gensoft <developer@gensoft.co.th>
2020-10-07 14:08:51 +0700

    Message commit

--- core-control/sms/report/report_notifywassent.php
+++ core-control/sms/report/report_notifywassent.php
@@ -2,7 +2,7 @@
 require_once('../../autoload.php');
 
 if($lib->checkCompleteArgument(['unique_id'],$dataComing)){
-	if($func->check_permission_core($payload,'sms','reportsmssuccess')){
+	if($func->check_permission_core($payload,'sms','reportnotifysuccess')){
 		$arrayExecute = array();
 		$arrayAll = array();
 		if(isset($dataComing["id_template"]) && $dataComing["id_template"] != ''){
@@ -23,22 +23,30 @@ if($lib->checkCompleteArgument(['unique_id'],$dataComing)){
 		if(isset($dataComing["is_sendahead"]) && $dataComing["is_sendahead"] != ''){
 			$arrayExecute["is_sendahead"] = $dataComing["is_sendahead"];
 		}
-		$fetchReport = $conmysql->prepare("SELECT sms_message,member_no,tel_mobile,send_date,send_by
-											FROM smslogwassent WHERE 1=1
+		$fetchReport = $conmysql->prepare("SELECT id_history, his_type, his_title, his_detail, his_path_image, his_read_status, his_del_status, member_no, receive_date, read_date, send_by, id_smstemplate, is_sendahead
+											FROM gchistory WHERE 1=1
 											".(isset($dataComing["id_template"]) && $dataComing["id_template"] != '' ? "and id_smstemplate = :id_template" : null)."
 											".(isset($dataComing["member_no"]) && $dataComing["member_no"] != '' ? "and member_no = :member_no" : null)."
 											".(isset($dataComing["send_by"]) && $dataComing["send_by"] != '' ? "and send_by = :send_by" : null)."
 											".(isset($dataComing["is_sendahead"]) && $dataComing["is_sendahead"] != '' ? "and is_sendahead = :is_sendahead" : null)."
-											".(isset($dataComing["start_date"]) && $dataComing["start_date"] != '' ? "and date_format(send_date,'%Y-%m-%d') >= :start_date" : null)."
-											".(isset($dataComing["end_date"]) && $dataComing["end_date"] != '' ? "and date_format(send_date,'%Y-%m-%d') <= :end_date" : null)." ORDER BY send_date DESC");
+											".(isset($dataComing["start_date"]) && $dataComing["start_date"] != '' ? "and date_format(receive_date,'%Y-%m-%d') >= :start_date" : null)."
+											".(isset($dataComing["end_date"]) && $dataComing["end_date"] != '' ? "and date_format(receive_date,'%Y-%m-%d') <= :end_date" : null)." ORDER BY receive_date DESC");
 		$fetchReport->execute($arrayExecute);
 		while($rowReport = $fetchReport->fetch(PDO::FETCH_ASSOC)){
 			$arrayReport = array();
-			$arrayReport["SMS_MESSAGE"] = $rowReport["sms_message"] ?? null;
+			$arrayReport["ID_HISTORY"] = $rowReport["id_history"] ?? null;
+			$arrayReport["HIS_TYPE"] = $rowReport["his_type"] ?? null;
+			$arrayReport["HIS_TITLE"] = $rowReport["his_title"] ?? null;
+			$arrayReport["HIS_DETAIL"] = $rowReport["his_detail"] ?? null;
+			$arrayReport["HIS_PATH_IMAGE"] = $rowReport["his_path_image"] ?? null;
+			$arrayReport["HIS_READ_STATUS"] = $rowReport["his_read_status"] ?? null;
+			$arrayReport["HIS_DEL_STATUS"] = $rowReport["his_del_status"] ?? null;
 			$arrayReport["MEMBER_NO"] = $rowReport["member_no"] ?? null;
-			$arrayReport["TEL_MOBILE"] = $lib->formatphone($rowReport["tel_mobile"],'-');
-			$arrayReport["SEND_DATE"] = isset($rowReport["send_date"]) ? $lib->convertdate($rowReport["send_date"],'d m Y',true) : null;
+			$arrayReport["RECEIVE_DATE"] = isset($rowReport["receive_date"]) ? $lib->convertdate($rowReport["receive_date"],'d m Y',true) : null;
+			$arrayReport["READ_DATE"] = isset($rowReport["read_date"]) ? $lib->convertdate($rowReport["read_date"],'d m Y',true) : null;
 			$arrayReport["SEND_BY"] = $rowReport["send_by"] ?? null;
+			$arrayReport["ID_SMSTEMPLATE"] = $rowReport["id_smstemplate"] ?? null;
+			$arrayReport["IS_SENDAHEAD"] = $rowReport["is_sendahead"] ?? null;
 			$arrayAll[] = $arrayReport;
 		}
 		$arrayResult['LIST_REPORT'] = $arrayAll;
---
+++ external_service/notify_req_assist.bat
@@ -0,0 +1,3 @@
+@echo off
+
+C:\Mobile\php7.4\php.exe C:\Mobile\service-crh\external_service\notification_req_assist_trigger.php
