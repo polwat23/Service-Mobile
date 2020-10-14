b64662e5b681c37498f96bee0a1b1710e0acaaba
dev_gensoft <developer@gensoft.co.th>
2020-10-14 14:21:16 +0700

    Edit demo service mju-test

--- mobile_and_web-control/receipt/report_receipt.php
+++ mobile_and_web-control/receipt/report_receipt.php
@@ -214,10 +214,10 @@ function GenerateReport($dataReport,$header,$lib){
 	}else{
 		$html .= '<p style="margin-top: -5px;font-size: 22px;font-weight: bold">ใบเสร็จรับเงิน</p>';
 	}
-	$html .= '<p style="margin-top: -30px;font-size: 22px;font-weight: bold">สหกรณ์ออมทรัพย์สามัญศึกษานครสวรรค์ จํากัด</p>
-				<p style="margin-top: -27px;font-size: 18px;">เลขที่ จ. 55/96-98 ถ.ดาวดึงส์</p>
-				<p style="margin-top: -25px;font-size: 18px;">ต ปากน้ำโพ อ.เมือง จ.นครสวรรค์</p>
-				<p style="margin-top: -25px;font-size: 18px;">โทร. 0-5622-2046</p>
+	$html .= '<p style="margin-top: -30px;font-size: 22px;font-weight: bold">สหกรณ์ออมทรัพย์มหาวิทยาลัยแม่โจ้ จำกัด</p>
+				<p style="margin-top: -27px;font-size: 18px;">63 หมู่ 4 ต.หนองหาร</p>
+				<p style="margin-top: -25px;font-size: 18px;">อ.สันทราย จ.เชียงใหม่  50290</p>
+				<p style="margin-top: -25px;font-size: 18px;">โทร. 053-878077 , 053-873000</p>
 				<p style="margin-top: -27px;font-size: 19px;font-weight: bold"></p>
 				</div>
 			</div>
