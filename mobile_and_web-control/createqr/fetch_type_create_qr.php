7b4bd0d3b1cd7fde9bbbe1a0b64e6029208a5709
dev_gensoft <developer@gensoft.co.th>
2020-11-26 11:34:51 +0700

    create qrcode for crossbank

---
+++ mobile_and_web-control/createqr/fetch_type_create_qr.php
@@ -0,0 +1,61 @@
+<?php
+require_once('../autoload.php');
+
+if($lib->checkCompleteArgument(['menu_component'],$dataComing)){
+	if($func->check_permission($payload["user_type"],$dataComing["menu_component"],'GenerateQR')){
+		$member_no = $configAS[$payload["member_no"]] ?? $payload["member_no"];
+		$arrayGrpTrans = array();
+		$arrGrpAcc = array();
+		$getTypeTransQR = $conmysql->prepare("SELECT trans_code_qr,trans_desc_qr,operation_desc_th,operation_desc_en FROM gcconttypetransqrcode WHERE is_use = '1'");
+		$getTypeTransQR->execute();
+		while($rowTypeQR = $getTypeTransQR->fetch(PDO::FETCH_ASSOC)){
+			$arrTypeQR = array();
+			$arrTypeQR["TRANS_CODE"] = $rowTypeQR["trans_code_qr"];
+			$arrTypeQR["TRANS_DESC"] = $rowTypeQR["trans_desc_qr"];
+			$arrTypeQR["OPERATE_DESC"] = $rowTypeQR["operation_desc_".$lang_locale];
+			$arrayGrpTrans[] = $arrTypeQR;
+			if($rowTypeQR["trans_code_qr"] == '01'){
+				$getAccountinTrans = $conoracle->prepare("SELECT DEPTACCOUNT_NO,DEPTACCOUNT_NAME,PRNCBAL FROM dpdeptmaster WHERE member_no = :member_no and deptclose_status <> 1");
+				$getAccountinTrans->execute([':member_no' => $member_no]);
+				while($rowAccTrans = $getAccountinTrans->fetch(PDO::FETCH_ASSOC)){
+					$arrAccTrans = array();
+					$arrAccTrans["ACCOUNT_NO"] = $lib->formataccount($rowAccTrans["DEPTACCOUNT_NO"],$func->getConstant('dep_format'));
+					$arrAccTrans["ACCOUNT_NO_HIDE"] = $lib->formataccount_hidden($arrAccTrans["ACCOUNT_NO"],$func->getConstant('hidden_dep'));
+					$arrAccTrans["ACCOUNT_NAME"] = TRIM($rowAccTrans["DEPTACCOUNT_NAME"]);
+					$arrAccTrans["PRIN_BAL"] = $rowAccTrans["PRNCBAL"];
+					$arrAccTrans["TRANS_CODE"] = $rowTypeQR["trans_code_qr"];
+					$arrGrpAcc[] = $arrAccTrans;
+				}
+			}
+		}
+		$arrayResult["TYPE_TRANS"] = $arrayGrpTrans;
+		$arrayResult["CHOOSE_ACCOUNT"] = $arrGrpAcc;
+		$arrayResult["RESULT"] = TRUE;
+		echo json_encode($arrayResult);
+	}else{
+		$arrayResult['RESPONSE_CODE'] = "WS0006";
+		$arrayResult['RESPONSE_MESSAGE'] = $configError[$arrayResult['RESPONSE_CODE']][0][$lang_locale];
+		$arrayResult['RESULT'] = FALSE;
+		http_response_code(403);
+		echo json_encode($arrayResult);
+		exit();
+	}
+}else{
+	$filename = basename(__FILE__, '.php');
+	$logStruc = [
+		":error_menu" => $filename,
+		":error_code" => "WS4004",
+		":error_desc" => "ส่ง Argument มาไม่ครบ "."\n".json_encode($dataComing),
+		":error_device" => $dataComing["channel"].' - '.$dataComing["unique_id"].' on V.'.$dataComing["app_version"]
+	];
+	$log->writeLog('errorusage',$logStruc);
+	$message_error = "ไฟล์ ".$filename." ส่ง Argument มาไม่ครบมาแค่ "."\n".json_encode($dataComing);
+	$lib->sendLineNotify($message_error);
+	$arrayResult['RESPONSE_CODE'] = "WS4004";
+	$arrayResult['RESPONSE_MESSAGE'] = $configError[$arrayResult['RESPONSE_CODE']][0][$lang_locale];
+	$arrayResult['RESULT'] = FALSE;
+	http_response_code(400);
+	echo json_encode($arrayResult);
+	exit();
+}
+?>
--- mobile_and_web-control/history/fetch_history_badge_number.php
+++ mobile_and_web-control/history/fetch_history_badge_number.php
@@ -2,58 +2,58 @@
 require_once('../autoload.php');
 
 if($lib->checkCompleteArgument(['menu_component'],$dataComing)){
-	if($func->check_permission($payload["user_type"],$dataComing["menu_component"],'Notification')){
-		$getBadge = $conmysql->prepare("SELECT IFNULL(COUNT(id_history),0) as badge,his_type FROM gchistory 
-										WHERE member_no = :member_no AND his_read_status = '0'
-										GROUP BY his_type");
-		$getBadge->execute([
-			':member_no' => $payload["member_no"]
-		]);
-		if($getBadge->rowCount() > 0){
-			while($badgeData = $getBadge->fetch(PDO::FETCH_ASSOC)){
-				$arrayResult['BADGE_'.$badgeData["his_type"]] = isset($badgeData["badge"]) ? $badgeData["badge"] : 0;
-			}
-			if(isset($arrayResult['BADGE_1'])){
-				$arrayResult['BADGE_1'] = $arrayResult['BADGE_1'];
-			}else{
-				$arrayResult['BADGE_1'] = 0;
-			}
-			if(isset($arrayResult['BADGE_2'])){
-				$arrayResult['BADGE_2'] = $arrayResult['BADGE_2'];
-			}else{
-				$arrayResult['BADGE_2'] = 0;
-			}
-			$arrayResult['BADGE_SUMMARY'] = $arrayResult['BADGE_1'] + $arrayResult['BADGE_2'];
-			$arrayResult['RESULT'] = TRUE;
-			echo json_encode($arrayResult);
-		}else{
-			http_response_code(204);
-			exit();
-		}
-	}else{
-		$arrayResult['RESPONSE_CODE'] = "WS0006";
-		$arrayResult['RESPONSE_MESSAGE'] = $configError[$arrayResult['RESPONSE_CODE']][0][$lang_locale];
-		$arrayResult['RESULT'] = FALSE;
-		http_response_code(403);
-		echo json_encode($arrayResult);
-		exit();
-	}
+if($func->check_permission($payload["user_type"],$dataComing["menu_component"],'Notification')){
+$getBadge = $conmysql->prepare("SELECT IFNULL(COUNT(id_history),0) as badge,his_type FROM gchistory 
+WHERE member_no = :member_no AND his_read_status = '0' and his_del_status = '0' 
+GROUP BY his_type");
+$getBadge->execute([
+':member_no' => $payload["member_no"]
+]);
+if($getBadge->rowCount() > 0){
+while($badgeData = $getBadge->fetch(PDO::FETCH_ASSOC)){
+$arrayResult['BADGE_'.$badgeData["his_type"]] = isset($badgeData["badge"]) ? $badgeData["badge"] : 0;
+}
+if(isset($arrayResult['BADGE_1'])){
+$arrayResult['BADGE_1'] = $arrayResult['BADGE_1'];
 }else{
-	$filename = basename(__FILE__, '.php');
-	$logStruc = [
-		":error_menu" => $filename,
-		":error_code" => "WS4004",
-		":error_desc" => "ส่ง Argument มาไม่ครบ "."\n".json_encode($dataComing),
-		":error_device" => $dataComing["channel"].' - '.$dataComing["unique_id"].' on V.'.$dataComing["app_version"]
-	];
-	$log->writeLog('errorusage',$logStruc);
-	$message_error = "ไฟล์ ".$filename." ส่ง Argument มาไม่ครบมาแค่ "."\n".json_encode($dataComing);
-	$lib->sendLineNotify($message_error);
-	$arrayResult['RESPONSE_CODE'] = "WS4004";
-	$arrayResult['RESPONSE_MESSAGE'] = $configError[$arrayResult['RESPONSE_CODE']][0][$lang_locale];
-	$arrayResult['RESULT'] = FALSE;
-	http_response_code(400);
-	echo json_encode($arrayResult);
-	exit();
+$arrayResult['BADGE_1'] = 0;
+}
+if(isset($arrayResult['BADGE_2'])){
+$arrayResult['BADGE_2'] = $arrayResult['BADGE_2'];
+}else{
+$arrayResult['BADGE_2'] = 0;
+}
+$arrayResult['BADGE_SUMMARY'] = $arrayResult['BADGE_1'] + $arrayResult['BADGE_2'];
+$arrayResult['RESULT'] = TRUE;
+echo json_encode($arrayResult);
+}else{
+http_response_code(204);
+exit();
+}
+}else{
+$arrayResult['RESPONSE_CODE'] = "WS0006";
+$arrayResult['RESPONSE_MESSAGE'] = $configError[$arrayResult['RESPONSE_CODE']][0][$lang_locale];
+$arrayResult['RESULT'] = FALSE;
+http_response_code(403);
+echo json_encode($arrayResult);
+exit();
+}
+}else{
+$filename = basename(__FILE__, '.php');
+$logStruc = [
+":error_menu" => $filename,
+":error_code" => "WS4004",
+":error_desc" => "ส่ง Argument มาไม่ครบ "."\n".json_encode($dataComing),
+":error_device" => $dataComing["channel"].' - '.$dataComing["unique_id"].' on V.'.$dataComing["app_version"]
+];
+$log->writeLog('errorusage',$logStruc);
+$message_error = "ไฟล์ ".$filename." ส่ง Argument มาไม่ครบมาแค่ "."\n".json_encode($dataComing);
+$lib->sendLineNotify($message_error);
+$arrayResult['RESPONSE_CODE'] = "WS4004";
+$arrayResult['RESPONSE_MESSAGE'] = $configError[$arrayResult['RESPONSE_CODE']][0][$lang_locale];
+$arrayResult['RESULT'] = FALSE;
+http_response_code(400);
+echo json_encode($arrayResult);
+exit();
 }
 ?>
