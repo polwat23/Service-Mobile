da84f1dbacdb82093570844671d6bd29596428b7
dev_gensoft <developer@gensoft.co.th>
2020-12-03 14:39:12 +0700

    Stable deploy v1.5

--- core-control/admin_control/managemenuandpermission/fetch_core_user.php
+++ core-control/admin_control/managemenuandpermission/fetch_core_user.php
@@ -13,15 +13,15 @@ if($lib->checkCompleteArgument(['unique_id'],$dataComing)){
 		}
 		$arrayResult["CORE_USER"] = $arrayGroup;
 		$arrayResult["RESULT"] = TRUE;
-		require_once('../../../../include/exit_footer.php');
+		require_once('../../../include/exit_footer.php');
 	}else{
 		$arrayResult['RESULT'] = FALSE;
 		http_response_code(403);
-		require_once('../../../../include/exit_footer.php');
+		require_once('../../../include/exit_footer.php');
 	}
 }else{
 	$arrayResult['RESULT'] = FALSE;
 	http_response_code(400);
-	require_once('../../../../include/exit_footer.php');
+	require_once('../../../include/exit_footer.php');
 }
 ?>
