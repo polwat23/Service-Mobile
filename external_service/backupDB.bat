92cd43658339e35900c7597ec9d4df6eb6a8d466
dev_gensoft <developer@gensoft.co.th>
2020-12-05 13:09:50 +0700

    edit external service

--- external_service/backupDB.bat
+++ external_service/backupDB.bat
@@ -1,22 +1,22 @@
 @echo off
 
 
-del C:\Mobile\service-tak\external_service\backupDB_tak.zip
+del C:\Mobile\service-ktscc\external_service\backupDB_ktscc.zip
 
 cd C:\Program Files\MariaDB 10.4\bin
 
 C:
 
-mysqldump.exe -hlocalhost -P3306 -u root -p@TAK2020 mobile_tak > C:\Mobile\service-tak\external_service\backupDB_tak.sql
+mysqldump.exe -hlocalhost -P3306 -u root -p@KTSCC2020 mobile_ktscc > C:\Mobile\service-ktscc\external_service\backupDB_ktscc.sql
 
-"C:\Program Files\7-Zip\7z.exe" a -r C:\Mobile\service-tak\external_service\backupDB_tak.zip C:\Mobile\service-tak\external_service\backupDB_tak.sql
+"C:\Program Files\7-Zip\7z.exe" a -r C:\Mobile\service-ktscc\external_service\backupDB_ktscc.zip C:\Mobile\service-ktscc\external_service\backupDB_ktscc.sql
 
-del C:\Mobile\service-tak\external_service\backupDB_tak.sql
+del C:\Mobile\service-ktscc\external_service\backupDB_ktscc.sql
 
 cd C:\Program Files (x86)\WinSCP
 
 C:
 
-winscp.exe /command "open ftp://ftp_backup:@Gensoft2018@203.154.140.14/incoming" "put C:\Mobile\service-tak\external_service\backupDB_tak.zip" "exit"
+winscp.exe /command "open ftp://ftp_backup:@Gensoft2018@203.154.140.14/incoming" "put C:\Mobile\service-ktscc\external_service\backupDB_ktscc.zip" "exit"
 
-del C:\Mobile\service-tak\external_service\backupDB_tak.zip
+del C:\Mobile\service-ktscc\external_service\backupDB_ktscc.zip
