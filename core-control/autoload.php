d4fdeb4df472f077c3b2247a800a5dd58a9ae54e
dev_gensoft <developer@gensoft.co.th>
2020-12-07 10:10:56 +0700

    edit autoload nsth-test

--- core-control/autoload.php
+++ core-control/autoload.php
@@ -68,33 +68,33 @@ if ($_SERVER['REQUEST_METHOD'] !== 'OPTIONS') {
 				if(!$lib->checkCompleteArgument(['section_system','username','exp'],$payload)){
 					$arrayResult['RESULT'] = FALSE;
 					http_response_code(400);
-					require_once('../include/exit_footer.php');
+					require_once(__DIR__.'/../include/exit_footer.php');
 				}
 			}catch (ValidateException $e) {
 				$errorCode = $e->getCode();
 				if($errorCode === 3){
 					$arrayResult['RESULT'] = FALSE;
 					http_response_code(401);
-					require_once('../include/exit_footer.php');
+					require_once(__DIR__.'/../include/exit_footer.php');
 				}else if($errorCode === 4){
 					$arrayResult['RESULT'] = FALSE;
 					http_response_code(401);
-					require_once('../include/exit_footer.php');
+					require_once(__DIR__.'/../include/exit_footer.php');
 				}else{
 					$arrayResult['RESULT'] = FALSE;
 					http_response_code(401);
-					require_once('../include/exit_footer.php');
+					require_once(__DIR__.'/../include/exit_footer.php');
 				}
 			}
 		}else{
 			$arrayResult['RESULT'] = FALSE;
 			http_response_code(400);
-			require_once('../include/exit_footer.php');
+			require_once(__DIR__.'/../include/exit_footer.php');
 		}
 	}
 }else{
 	$arrayResult['RESULT'] = TRUE;
 	http_response_code(203);
-	require_once('../include/exit_footer.php');
+	require_once(__DIR__.'/../include/exit_footer.php');
 }
 ?>
