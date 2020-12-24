3ee8311f63e9aed62bf7b85b6f73e5ffd96c67ad
dev_gensoft <developer@gensoft.co.th>
2020-12-24 15:34:47 +0700

    fix fetch loanred/ manage document

--- core-control/mobile_admin/moremobileadmin/reqloanform/fetch_reqloan_form_online.php
+++ core-control/mobile_admin/moremobileadmin/reqloanform/fetch_reqloan_form_online.php
@@ -6,6 +6,7 @@ if($lib->checkCompleteArgument(['unique_id'],$dataComing)){
 		$arrGrp = array();
 		$arrayType = array();
 		$arrayExecute = array();
+		$formatDept = $func->getConstant('dep_format');
 		$getLoanTypeDesc = $conoracle->prepare("SELECT loantype_desc,loantype_code FROM lnloantype");
 		$getLoanTypeDesc->execute();
 		while($rowType = $getLoanTypeDesc->fetch(PDO::FETCH_ASSOC)){
@@ -28,7 +29,7 @@ if($lib->checkCompleteArgument(['unique_id'],$dataComing)){
 			$arrayExecute[':end_date'] = $dataComing["end_date"];
 		}
 		if(isset($dataComing["req_status"]) && $dataComing["req_status"] != ""){
-			$getAllReqDocno = $conmysql->prepare("SELECT reqloan_doc,member_no,loantype_code,request_amt,period_payment,period,loanpermit_amt,salary_img,citizen_img,req_status,request_date,approve_date,contractdoc_url
+			$getAllReqDocno = $conmysql->prepare("SELECT reqloan_doc,member_no,loantype_code,request_amt,period_payment,period,loanpermit_amt,salary_img,citizen_img,req_status,request_date,approve_date,contractdoc_url,deptaccount_no_coop,objective,pay_date,option_pay
 															FROM gcreqloan WHERE req_status = :req_status". 
 															($dataComing["is_filtered"] ? (
 															(isset($dataComing["filter_member_no"]) && $dataComing["filter_member_no"] != '' ? " and member_no = :filter_member_no" : null).
@@ -71,6 +72,10 @@ if($lib->checkCompleteArgument(['unique_id'],$dataComing)){
 				$arrDocno["SALARY_IMG"] = $rowDocno["salary_img"];
 				$arrDocno["CITIZEN_IMG"] = $rowDocno["citizen_img"];
 				$arrDocno["CONTRACTDOC_URL"] = $rowDocno["contractdoc_url"];
+				$arrDocno["DEPTACCOUNT_NO_COOP"] = $lib->formataccount($rowDocno["deptaccount_no_coop"],$formatDept);
+				$arrDocno["OBJECTIVE"] = $rowDocno["objective"];
+				$arrDocno["OPTION_PAY"] = $rowDocno["option_pay"] == '0' ? "คงต้น" : "คงยอด";
+				$arrDocno["PAY_DATE"] = $lib->convertdate($rowDocno["pay_date"],'d m Y');
 				$arrDocno["REQ_STATUS"]  = $rowDocno["req_status"];
 				if($rowDocno["req_status"] == '8'){
 					$arrDocno["REQ_STATUS_DESC"] = "รอลงรับ";
@@ -86,7 +91,7 @@ if($lib->checkCompleteArgument(['unique_id'],$dataComing)){
 				$arrGrp[] = $arrDocno;
 			}
 		}else{
-			$getAllReqDocno = $conmysql->prepare("SELECT reqloan_doc,member_no,loantype_code,request_amt,period_payment,period,loanpermit_amt,salary_img,citizen_img,req_status,request_date,approve_date,contractdoc_url
+			$getAllReqDocno = $conmysql->prepare("SELECT reqloan_doc,member_no,loantype_code,request_amt,period_payment,period,loanpermit_amt,salary_img,citizen_img,req_status,request_date,approve_date,contractdoc_url,deptaccount_no_coop,objective,pay_date,option_pay
 															FROM gcreqloan WHERE 1=1". 
 															($dataComing["is_filtered"] ? (
 															(isset($dataComing["filter_member_no"]) && $dataComing["filter_member_no"] != '' ? " and member_no = :filter_member_no" : null).
@@ -129,6 +134,10 @@ if($lib->checkCompleteArgument(['unique_id'],$dataComing)){
 				$arrDocno["SALARY_IMG"] = $rowDocno["salary_img"];
 				$arrDocno["CITIZEN_IMG"] = $rowDocno["citizen_img"];
 				$arrDocno["CONTRACTDOC_URL"] = $rowDocno["contractdoc_url"];
+				$arrDocno["DEPTACCOUNT_NO_COOP"] = $lib->formataccount($rowDocno["deptaccount_no_coop"],$formatDept);
+				$arrDocno["OBJECTIVE"] = $rowDocno["objective"];
+				$arrDocno["OPTION_PAY"] = $rowDocno["option_pay"] == '0' ? "คงต้น" : "คงยอด";
+				$arrDocno["PAY_DATE"] = $lib->convertdate($rowDocno["pay_date"],'d m Y');
 				$arrDocno["REQ_STATUS"]  = $rowDocno["req_status"];
 				if($rowDocno["req_status"] == '8'){
 					$arrDocno["REQ_STATUS_DESC"] = "รอลงรับ";
