b47696877c943c30af1392f39bbced24e00c385c
dev_gensoft <developer@gensoft.co.th>
2020-12-25 15:54:33 +0700

    edit webshow_flag

--- external_service/clear_password_wrong.bat
+++ external_service/clear_password_wrong.bat
@@ -1,3 +1,3 @@
 @echo off
 
-C:\Mobile\php7.4\php.exe C:\Mobile\service-ktscc\external_service\clear_password_wrong.php
+C:\Mobile\php7.4\php.exe C:\Mobile\service-nan\external_service\clear_password_wrong.php
--- external_service/time_forward_event_notify.bat
+++ external_service/time_forward_event_notify.bat
@@ -1,3 +1,3 @@
 @echo off
 
-C:\Mobile\php7.4\php.exe C:\Mobile\service-ktscc\external_service\time_scheduler_event_notify.php
+C:\Mobile\php7.4\php.exe C:\Mobile\service-nan\external_service\time_scheduler_event_notify.php
