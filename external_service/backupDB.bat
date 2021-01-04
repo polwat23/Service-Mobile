a2e4168b834769553df8344626c9c16eff642f19
dev_gensoft <developer@gensoft.co.th>
2021-01-04 15:06:45 +0700

    edit notify

--- external_service/backupDB.bat
+++ external_service/backupDB.bat
@@ -1,15 +1,16 @@
 @echo off
 
 
-del C:\Mobile\Service-MHD\external_service\backupDB_mhd.zip
+del C:\Mobile\Service-MJU\external_service\backupDB_MJU.zip
 
-cd C:\Program Files\MariaDB 10.4\bin & mysqldump.exe -hlocalhost -P3306 -u root -p@MUsaving2020 mobile_mhd > C:\Mobile\Service-MHD\external_service\backupDB_mhd.sql
+cd C:\Program Files\MariaDB 10.4\bin & mysqldump.exe -hlocalhost -P3306 -u root -p@MJU2020 mobile_MJU > C:\Mobile\Service-MJU\external_service\backupDB_MJU.sql
 
-"C:\Program Files\7-Zip\7z.exe" a -r C:\Mobile\Service-MHD\external_service\backupDB_mhd.zip C:\Mobile\Service-MHD\external_service\backupDB_mhd.sql
+"C:\Program Files\7-Zip\7z.exe" a -r C:\Mobile\Service-MJU\external_service\backupDB_MJU.zip C:\Mobile\Service-MJU\external_service\backupDB_MJU.sql
 
-del C:\Mobile\Service-MHD\external_service\backupDB_mhd.sql
+del C:\Mobile\Service-MJU\external_service\backupDB_MJU.sql
 
-ftp -i -s:C:\Mobile\Service-MHD\external_service\ftp_upload.bat
+cd C:\Program Files (x86)\WinSCP
 
-del C:\Mobile\Service-MHD\external_service\backupDB_mhd.zip
+winscp.exe /command "open ftp://ftp_backup:@Gensoft2018@203.154.140.14/incoming" "put C:\Mobile\service-MJU\external_service\backupDB_MJU.zip" "exit"
 
+del C:\Mobile\Service-MJU\external_service\backupDB_MJU.zip
