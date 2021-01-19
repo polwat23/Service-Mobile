330d80997647367568e1e174fba2d9f73e95b0de
dev_gensoft <developer@gensoft.co.th>
2021-01-19 12:03:25 +0700

    deploy v1

--- external_service/time_forward_event_notify.bat
+++ external_service/time_forward_event_notify.bat
@@ -1,3 +1,3 @@
 @echo off
 
-C:\Mobile\php7.4\php.exe C:\Mobile\service-tak\external_service\time_scheduler_event_notify.php
+C:\Mobile\php7.4\php.exe C:\Mobile\service-mhs\external_service\time_scheduler_event_notify.php
