8b37339daba7904d871f33ce8b3c211fcf611414
dev_gensoft <developer@gensoft.co.th>
2021-01-28 09:36:07 +0700

    fix serch member

--- core-control/mobile_admin/manageuser/searchmember/fetch_search_member.php
+++ core-control/mobile_admin/manageuser/searchmember/fetch_search_member.php
@@ -25,21 +25,21 @@ if($lib->checkCompleteArgument(['unique_id'],$dataComing)){
 			$arrayResult['RESULT'] = FALSE;
 			require_once('../../../../include/exit_footer.php');
 		}
-		$fetchMember = $conoracle->prepare("SELECT mp.prename_short,mb.memb_name,mb.memb_surname,mb.birth_date,mb.addr_email as email,mb.mem_telmobile as MEM_TELMOBILE,
+		$fetchMember = $conoracle->prepare("SELECT mp.prename_short,mb.memb_name,mb.memb_surname,mb.birth_date,mb.email as email,mb.mem_telmobile as MEM_TELMOBILE,
 											mb.member_date,mb.member_no,
-											mb.ADDR_NO as ADDR_NO,
-											mb.ADDR_MOO as ADDR_MOO,
-											mb.ADDR_SOI as ADDR_SOI,
-											mb.ADDR_VILLAGE as ADDR_VILLAGE,
-											mb.ADDR_ROAD as ADDR_ROAD,
+											mb.ADDRESS_NO as ADDR_NO,
+											mb.ADDRESS_MOO as ADDR_MOO,
+											mb.ADDRESS_SOI as ADDR_SOI,
+											mb.ADDRESS_VILLAGE as ADDR_VILLAGE,
+											mb.ADDRESS_ROAD as ADDR_ROAD,
 											MBT.TAMBOL_DESC AS TAMBOL_DESC,
 											MBD.DISTRICT_DESC AS DISTRICT_DESC,
 											MB.PROVINCE_CODE,
 											MBP.PROVINCE_DESC AS PROVINCE_DESC,
-											MB.ADDR_POSTCODE AS ADDR_POSTCODE
+											MB.POSTCODE AS ADDR_POSTCODE
 											FROM mbmembmaster mb LEFT JOIN mbucfprename mp ON mb.prename_code = mp.prename_code
 											LEFT JOIN mbucftambol MBT ON mb.tambol_code = MBT.tambol_code
-											LEFT JOIN mbucfdistrict MBD ON mb.AMPHUR_CODE = MBD.district_code
+											LEFT JOIN mbucfdistrict MBD ON mb.DISTRICT_CODE = MBD.district_code
 											LEFT JOIN mbucfprovince MBP ON mb.province_code = MBP.province_code
 											WHERE 1=1".(isset($dataComing["member_no"]) && $dataComing["member_no"] != '' ? " and mb.member_no = :member_no" : null).
 											(isset($dataComing["member_name"]) && $dataComing["member_name"] != '' ? " and (TRIM(mb.memb_name) LIKE :member_name" : null).
