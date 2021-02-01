ad931f9742270d5c2ec26baadd0937972cd35bc1
dev_gensoft <developer@gensoft.co.th>
2021-02-01 18:06:50 +0700

    add constantbank

--- core-control/mobile_admin/constmobileadmin/constantbankaccount/insert_bankconstant.php
+++ core-control/mobile_admin/constmobileadmin/constantbankaccount/insert_bankconstant.php
@@ -3,15 +3,21 @@ require_once('../../../autoload.php');
 
 if($lib->checkCompleteArgument(['unique_id'],$dataComing)){
 	if($func->check_permission_core($payload,'mobileadmin','constantbankaccount')){
-		$updateConstants = $conmysql->prepare("UPDATE gcbankconstant
-		SET transaction_cycle= :transaction_cycle,
-		max_numof_deposit= :max_numof_deposit,
-		max_numof_withdraw= :max_numof_withdraw,
-		min_deposit= :min_deposit,
-		max_deposit= :max_deposit,
-		min_withdraw= :min_withdraw,
-		max_withdraw= :max_withdraw
-		WHERE id_bankconstant = :id_bankconstant");
+		$updateConstants = $conmysql->prepare("INSERT INTO gcbankconstant
+		(transaction_cycle,
+		max_numof_deposit,
+		max_numof_withdraw,
+		min_deposit,
+		max_deposit,
+		min_withdraw,
+		max_withdraw)
+		VALUES (:transaction_cycle,
+		:max_numof_deposit,
+		:max_numof_withdraw,
+		:min_deposit,
+		:max_deposit,
+		:min_withdraw,
+		:max_withdraw)");
 		if($updateConstants->execute([
 			':transaction_cycle' => $dataComing["transaction_cycle"],
 			':max_numof_deposit' => $dataComing["max_numof_deposit"],
@@ -19,13 +25,12 @@ if($lib->checkCompleteArgument(['unique_id'],$dataComing)){
 			':min_deposit' => $dataComing["min_deposit"],
 			':max_deposit' => $dataComing["max_deposit"],
 			':min_withdraw' => $dataComing["min_withdraw"],
-			':max_withdraw' => $dataComing["max_withdraw"],
-			':id_bankconstant' => $dataComing["id_bankconstant"]
+			':max_withdraw' => $dataComing["max_withdraw"]
 		])){
 			$arrayStruc = [
 					':menu_name' => "constantbankaccount",
 					':username' => $payload["username"],
-					':use_list' =>"update gcbankconstant",
+					':use_list' =>"insert gcbankconstant",
 					':details' => "transaction_cycle => ".$dataComing["transaction_cycle"].
 								" max_numof_deposit => ".$dataComing["max_numof_deposit"].
 								" max_numof_withdraw => ".$dataComing["max_numof_withdraw"].
@@ -38,7 +43,7 @@ if($lib->checkCompleteArgument(['unique_id'],$dataComing)){
 			$arrayResult["RESULT"] = TRUE;
 			echo json_encode($arrayResult);
 		}else{
-			$arrayResult['RESPONSE'] = "ไม่สามารถแก้ไขค่าคงที่ได้ กรุณาติดต่อผู้พัฒนา";
+			$arrayResult['RESPONSE'] = "ไม่สามารถเพิ่มค่าคงที่ได้ กรุณาติดต่อผู้พัฒนา";
 			$arrayResult['RESULT'] = FALSE;
 			echo json_encode($arrayResult);
 			exit();
