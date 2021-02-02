5718ee6697081c6c9337e2232392de78fae01adf
dev_gensoft <developer@gensoft.co.th>
2021-02-02 17:27:09 +0700

    edit dividend

--- mobile_and_web-control/dividend/fetch_dividend_info.php
+++ mobile_and_web-control/dividend/fetch_dividend_info.php
@@ -33,8 +33,8 @@ if($lib->checkCompleteArgument(['menu_component'],$dataComing)){
 			$getRateDiv->execute([':year' => $rowYear["DIV_YEAR"]]);
 			$rowRateDiv = $getRateDiv->fetch(PDO::FETCH_ASSOC);
 			$arrDividend["YEAR"] = $rowYear["DIV_YEAR"];
-			$arrDividend["DIV_RATE"] = $rowRateDiv["DIVIDEND_RATE"] * 100;
-			$arrDividend["AVG_RATE"] = $rowRateDiv["AVERAGE_RATE"] * 100;
+			$arrDividend["DIV_RATE"] = ($rowRateDiv["DIVIDEND_RATE"] * 100)." %";
+			$arrDividend["AVG_RATE"] = ($rowRateDiv["AVERAGE_RATE"] * 100)." %";
 			$arrDividend["DIV_AMT"] = number_format($rowDiv["DIV_AMT"],2);
 			$arrDividend["AVG_AMT"] = number_format($rowDiv["AVG_AMT"],2);
 			$arrDividend["SUM_AMT"] = number_format($rowDiv["SUMDIV"],2);
