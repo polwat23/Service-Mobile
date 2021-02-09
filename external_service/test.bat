e31cd35c67855e308f7f0bfeeef4f710118553bb
dev_gensoft <developer@gensoft.co.th>
2021-02-09 14:30:52 +0700

    edit insert his

--- external_service/test.bat
+++ external_service/test.bat
@@ -1,5 +1,5 @@
 
 
-C:\Mobile\php7.4\php.exe C:\Mobile\Service-MHD-Test\external_service\notification_share_trigger.php
+D:\php7.4\php.exe D:\Mobile\Service-ptt-Test\external_service\test.php
 
 pause
