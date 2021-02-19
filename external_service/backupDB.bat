a786b176dfdb167cab67af6f3b056d90b6bd4cc7
dev_gensoft <developer@gensoft.co.th>
2021-02-19 17:18:46 +0700

    edit version database

--- external_service/backupDB.bat
+++ external_service/backupDB.bat
@@ -1,9 +1,9 @@
-::@echo off
+@echo off
 
 
 del C:\Mobile\Service-ARD\external_service\backupDB_ard.zip
 
-cd C:\Program Files\MariaDB 10.4\bin & mysqldump.exe -hlocalhost -P3306 -u root -p@Ard2020 mobile_ard > C:\Mobile\Service-ARD\external_service\backupDB_ard.sql
+cd C:\Program Files\MariaDB 10.5\bin & mysqldump.exe -hlocalhost -P3306 -u root -p@Ard2020 mobile_ard > C:\Mobile\Service-ARD\external_service\backupDB_ard.sql
 
 "C:\Program Files\7-Zip\7z.exe" a -r C:\Mobile\Service-ARD\external_service\backupDB_ard.zip C:\Mobile\Service-ARD\external_service\backupDB_ard.sql
 
