0b28a7fcd7bbe793ce7cc0ae78dcd7b858a35f63
dev_gensoft <developer@gensoft.co.th>
2021-04-02 13:57:26 +0700

    edit notify

--- mobile_and_web-control/deposit/request_deposit_statement.php
+++ mobile_and_web-control/deposit/request_deposit_statement.php
@@ -189,10 +189,10 @@ function generatePDFSTM($dompdf,$arrayData,$lib,$password){
 	<div style="position:fixed;">
 			   <div style="padding:0px;"><img src="../../resource/logo/logo.jpg" style="width:50px "></div>
 			   <div style=" position: fixed;top:2px; left: 60px; font-size:20px; font-weight:bold;">
-					สหกรณ์ออมทรัพย์ครูหนองคาย จำกัด
+					สหกรณ์ออมทรัพย์ครูยโสธร จำกัด
 			   </div>
 			   <div style=" position: fixed;top:25px; left: 60px;font-size:20px">
-					Nongkhai Teacher savings and credit cooperative limited
+					Yasothon Teacher Saving Cooperatives Limited
 			   </div>
 			   </div>
 				<div class="frame-info-user">
--- mobile_and_web-control/receipt/report_receipt.php
+++ mobile_and_web-control/receipt/report_receipt.php
@@ -215,11 +215,11 @@ function GenerateReport($dataReport,$header,$lib){
 	}else{
 		$html .= '<p style="margin-top: -5px;font-size: 22px;font-weight: bold">ใบเสร็จรับเงิน</p>';
 	}
-	$html .= '<p style="margin-top: -30px;font-size: 22px;font-weight: bold">สหกรณ์ออมทรัพย์ครูหนองคาย จำกัด</p>
-			<p style="margin-top: -27px;font-size: 18px;">143 ถ.ประจักษ์ ต.ในเมือง อ.เมือง</p>
-			<p style="margin-top: -25px;font-size: 18px;">จังหวัดหนองคาย 43000</p>
-			<p style="margin-top: -25px;font-size: 18px;">โทร : 042-411-334</p>
-			<p style="margin-top: -27px;font-size: 19px;font-weight: bold">www.nktsc.org</p>
+	$html .= '<p style="margin-top: -30px;font-size: 22px;font-weight: bold">สหกรณ์ออมทรัพย์ครูยโสธร จำกัด</p>
+			<p style="margin-top: -27px;font-size: 18px;">272 ถ.เลี่ยงเมือง ต.ในเมือง </p>
+			<p style="margin-top: -25px;font-size: 18px;">อ.เมืองยโสธร จ.ยโสธร 35000</p>
+			<p style="margin-top: -25px;font-size: 18px;">โทร : 045-712758 ต่อ 11</p>
+			<p style="margin-top: -27px;font-size: 19px;font-weight: bold">www.yasocoop.in.th</p>
 			</div>
 			</div>
 			<div style="margin: 25px 0 10px 0;">
