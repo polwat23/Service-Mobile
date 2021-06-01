d0f1199797032430aee081579613e579acacd2c9
dev_gensoft <developer@gensoft.co.th>
2021-06-01 14:40:06 +0700

    stable deploy edit backlog

--- external_service/backupDB.bat
+++ external_service/backupDB.bat
@@ -3,7 +3,7 @@
 
 del C:\Mobile\service-pjt\external_service\backupDB_pjt.zip
 
-cd C:\Program Files\MariaDB 10.4\bin & mysqldump.exe -hlocalhost -P3306 -u root -p@PJT2020 mobile_pjt > C:\Mobile\service-pjt\external_service\backupDB_pjt.sql
+cd C:\Program Files\MariaDB 10.5\bin & mysqldump.exe -hlocalhost -P3306 -u root -p@PJT2020 mobile_pjt > C:\Mobile\service-pjt\external_service\backupDB_pjt.sql
 
 "C:\Program Files\7-Zip\7z.exe" a -r C:\Mobile\service-pjt\external_service\backupDB_pjt.zip C:\Mobile\service-pjt\external_service\backupDB_pjt.sql
 
