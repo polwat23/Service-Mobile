42f92115cf602a3b0c0b9c6deab605fb47741f8d
dev_gensoft <developer@gensoft.co.th>
2021-06-01 10:17:46 +0700

    add fullname qrcodelist

--- core-control/mobile_admin/moremobileadmin/qrgeneratelist/fetch_qrgenerate_list.php
+++ core-control/mobile_admin/moremobileadmin/qrgeneratelist/fetch_qrgenerate_list.php
@@ -4,6 +4,10 @@ require_once('../../../autoload.php');
 if($lib->checkCompleteArgument(['unique_id'],$dataComing)){
 	if($func->check_permission_core($payload,'mobileadmin','qrgeneratelist')){
 		$arrayGrpAll = array();
+		$arrayMemberGrp = array();
+		$arrayQueryMembNo = array();
+		$arrayCount = 0;
+		$arrayGrpQr = array();
 		
 		$fetchQrgenerateList = $conmysql->prepare("SELECT qrm.qrcodegen_id,qrm.qrgenerate,qrm.member_no,qrm.generate_date,qrm.qrtransfer_amt,qrm.qrtransfer_fee,
 										qrm.expire_date,qrm.transfer_status,qrm.update_date,
@@ -11,8 +15,7 @@ if($lib->checkCompleteArgument(['unique_id'],$dataComing)){
                                         qrc.trans_desc_qr
 										FROM gcqrcodegenmaster qrm
 										LEFT JOIN gcqrcodegendetail qrd ON qrd.qrgenerate = qrm.qrgenerate
-										LEFT JOIN gcconttypetransqrcode qrc ON qrd.trans_code_qr = qrc.trans_code_qr
-									");
+										LEFT JOIN gcconttypetransqrcode qrc ON qrd.trans_code_qr = qrc.trans_code_qr");
 			
 		$fetchQrgenerateList->execute();
 		while($rowQr = $fetchQrgenerateList->fetch(PDO::FETCH_ASSOC)){
@@ -20,6 +23,15 @@ if($lib->checkCompleteArgument(['unique_id'],$dataComing)){
 			$arrayQr["QRCODEGEN_ID"] = $rowQr["qrcodegen_id"];
 			$arrayQr["QRGENERATE"] = $rowQr["qrgenerate"];
 			$arrayQr["MEMBER_NO"] = $rowQr["member_no"];
+			if(isset($arrayMemberGrp[$rowQr["member_no"]])){
+				
+			}else{
+				if(isset($rowQr["member_no"]) && $rowQr["member_no"] != ""){
+					$arrayMemberGrp[$rowQr["member_no"]]["MEMBER_NO"] = $rowQr["member_no"];
+					$arrayQueryMembNo[] = $rowQr["member_no"];
+					$arrayCount++;
+				}
+			}
 			$arrayQr["GENERATE_DATE"] = $lib->convertdate($rowQr["generate_date"],'d m Y',true);
 			$arrayQr["QRTRANSFER_AMT"] = number_format($rowQr["qrtransfer_amt"],2);
 			$arrayQr["QRTRANSFER_FEE"] = number_format($rowQr["qrtransfer_fee"],2);
@@ -32,10 +44,38 @@ if($lib->checkCompleteArgument(['unique_id'],$dataComing)){
 			$arrayQr["QRTRANSFERDT_FEE"] = number_format($rowQr["qrtransferdt_fee"],2);
 			$arrayQr["TRANS_DESC_QR"] = $rowQr["trans_desc_qr"];
 			
+			if($arrayCount == 1000){
+				$memberInfo = $conoracle->prepare("SELECT mp.prename_short,mb.memb_name,mb.memb_surname,mb.member_no
+													FROM mbmembmaster mb LEFT JOIN mbucfprename mp ON mb.prename_code = mp.prename_code
+													WHERE mb.member_no IN('".implode("','",$arrayQueryMembNo)."')");
+				$memberInfo->execute();
+				while($rowMemberInfo = $memberInfo->fetch(PDO::FETCH_ASSOC)){
+					$arrayMemberGrp[$rowMemberInfo["MEMBER_NO"]]["FULLNAME"] = $rowMemberInfo["PRENAME_SHORT"].$rowMemberInfo["MEMB_NAME"]." ".$rowMemberInfo["MEMB_SURNAME"];
+				}
+				$arrayQueryMembNo = array();
+				$arrayCount = 0;
+			}
 			$arrayGrpAll[] = $arrayQr;
 		}
 		
-		$arrayResult['QrGenerateList'] = $arrayGrpAll;
+		if($arrayCount > 0){
+			$memberInfo = $conoracle->prepare("SELECT mp.prename_short,mb.memb_name,mb.memb_surname,mb.member_no
+											FROM mbmembmaster mb LEFT JOIN mbucfprename mp ON mb.prename_code = mp.prename_code
+											WHERE mb.member_no IN('".implode("','",$arrayQueryMembNo)."')");
+			$memberInfo->execute();
+			while($rowMemberInfo = $memberInfo->fetch(PDO::FETCH_ASSOC)){
+				$arrayMemberGrp[$rowMemberInfo["MEMBER_NO"]]["FULLNAME"] = $rowMemberInfo["PRENAME_SHORT"].$rowMemberInfo["MEMB_NAME"]." ".$rowMemberInfo["MEMB_SURNAME"];
+			}
+		}
+		
+		foreach ($arrayGrpAll as $value) {
+			$arrayQr = $value;
+			$arrayQr["FULLNAME"] = $arrayMemberGrp[$value["MEMBER_NO"]]["FULLNAME"];
+			$arrayGrpQr[] = $arrayQr;
+		}
+		
+		$arrayResult['QrGenerateList'] = $arrayGrpQr;
+		$arrayResult['i'] = $memberInfo;
 		$arrayResult['RESULT'] = TRUE;
 		require_once('../../../../include/exit_footer.php');
 	}else{
