9f8618b0b4ddede205473464177177a3934605db
dev_gensoft <developer@gensoft.co.th>
2021-06-04 11:26:35 +0700

    edit stable service from backlog

--- mobile_and_web-control/keeping/report_keeping_monthly.php
+++ mobile_and_web-control/keeping/report_keeping_monthly.php
@@ -138,17 +138,18 @@ function GenerateReport($dataReport,$header,$lib){
 	$sumBalance = 0;
 	$html = '<style>
 				@font-face {
-				  font-family: THSarabun;
-				  src: url(../../resource/fonts/THSarabun.ttf);
+				  font-family: TH Niramit AS;
+				  src: url(../../resource/fonts/TH Niramit AS.ttf);
 				}
 				@font-face {
-					font-family: "THSarabun";
-					src: url(../../resource/fonts/THSarabun Bold.ttf);
+					font-family: TH Niramit AS;
+					src: url(../../resource/fonts/TH Niramit AS Bold.ttf);
 					font-weight: bold;
 				}
 				* {
-				  font-family: THSarabun;
+				  font-family: TH Niramit AS;
 				}
+
 				body {
 				  padding: 0 30px;
 				}
@@ -275,7 +276,12 @@ function GenerateReport($dataReport,$header,$lib){
 			<p style="margin-left: 50px;">เจ้าหน้าที่รับเงิน</p></div>
 			';
 
-	$dompdf = new DOMPDF();
+	$dompdf = new Dompdf([
+		'fontDir' => realpath('../../resource/fonts'),
+		'chroot' => realpath('/'),
+		'isRemoteEnabled' => true
+	]);
+
 	$dompdf->set_paper('A4', 'landscape');
 	$dompdf->load_html($html);
 	$dompdf->render();
