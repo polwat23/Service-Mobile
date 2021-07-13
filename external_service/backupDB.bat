79813d983a95c552f8b60ae447ade138479765dc
dev_gensoft <developer@gensoft.co.th>
2021-07-13 11:10:12 +0700

    first deploy v3

--- external_service/backupDB.bat
+++ external_service/backupDB.bat
@@ -1,24 +1,24 @@
 @echo off
 
 
-del C:\Mobile\service-stk\external_service\backupDB_stk.zip
+del C:\Mobile\service-slp\external_service\backupDB_slp.zip
 
-cd C:\Program Files\MariaDB 10.4\bin
+cd C:\Program Files\MariaDB 10.5\bin
 
 C:
 
-mysqldump.exe -hlocalhost -P3306 -u root -p@STK2021 mobile_stk > C:\Mobile\service-stk\external_service\backupDB_stk.sql
+mysqldump.exe -hlocalhost -P3306 -u root -p@SLP2021 mobile_slp > C:\Mobile\service-slp\external_service\backupDB_slp.sql
 
-"C:\Program Files\7-Zip\7z.exe" a -r C:\Mobile\service-stk\external_service\backupDB_stk.zip C:\Mobile\service-stk\external_service\backupDB_stk.sql
+"C:\Program Files\7-Zip\7z.exe" a -r C:\Mobile\service-slp\external_service\backupDB_slp.zip C:\Mobile\service-slp\external_service\backupDB_slp.sql
 
-del C:\Mobile\service-stk\external_service\backupDB_stk.sql
+del C:\Mobile\service-slp\external_service\backupDB_slp.sql
 
 cd C:\Program Files (x86)\WinSCP
 
 C:
 
-winscp.exe /command "open ftp://ftp_backup:@Gensoft2018@203.154.140.14/incoming" "put C:\Mobile\service-stk\external_service\backupDB_stk.zip" "exit"
+winscp.exe /command "open ftp://ftp_backup:@Gensoft2018@203.154.140.14/incoming" "put C:\Mobile\service-slp\external_service\backupDB_slp.zip" "exit"
 
-del C:\Mobile\service-stk\external_service\backupDB_stk.zip
+del C:\Mobile\service-slp\external_service\backupDB_slp.zip
 
 pause
---
+++ external_service/time_forward_event_notify.bat
@@ -0,0 +1,3 @@
+@echo off
+
+C:\Mobile\php7.4\php.exe C:\Mobile\Service-SLP\external_service\time_scheduler_event_notify.php
