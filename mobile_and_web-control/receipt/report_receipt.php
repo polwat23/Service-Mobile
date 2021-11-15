a77220bc2f30cc8aee539a7476d61c816b40c0b3
dev_gensoft <developer@gensoft.co.th>
2021-11-15 16:38:39 +0700

    edit signature and logo keeping

--- mobile_and_web-control/receipt/report_receipt.php
+++ mobile_and_web-control/receipt/report_receipt.php
@@ -324,15 +324,15 @@ function GenerateReport($dataReport,$header,$lib){
 			<div style="display:flex;">
 			<div style="width:500px;font-size: 18px;">หมายเหตุ : ใบรับเงินประจำเดือนจะสมบูรณ์ก็ต่อเมื่อทางสหกรณ์ได้รับเงินที่เรียกเก็บเรียบร้อยแล้ว<br>ติดต่อสหกรณ์ โปรดนำ 1. บัตรประจำตัว 2. ใบเสร็จรับเงิน 3. สลิปเงินเดือนมาด้วยทุกครั้ง
 			</div>
-			<div style="width:200px;margin-left: 550px;display:flex;">
-			<img src="../../resource/utility_icon/signature/mg.png" width="80" height="50" style="margin-top:10px;"/>
+			<div style="width:200px;margin-left: 560px;display:flex;">
+			<img src="../../resource/utility_icon/signature/treasurer_ago.jpg" width="80" height="50" style="margin-top:10px;"/>
 			</div>
-			<div style="width:200px;margin-left: 750px;display:flex;">
-			<img src="../../resource/utility_icon/signature/fn.png" width="100" height="50" style="margin-top:10px;"/>
+			<div style="width:200px;margin-left: 770px;display:flex;">
+			<img src="../../resource/utility_icon/signature/finance_ago.jpg" width="100" height="50" style="margin-top:10px;"/>
 			</div>
 			</div>
-			<div style="font-size: 18px;margin-left: 580px;margin-top:-90px;">ผู้จัดการ</div>
-			<div style="font-size: 18px;margin-left: 780px;margin-top:-90px;">เจ้าหน้าที่รับเงิน</div>
+			<div style="font-size: 18px;margin-left: 580px;margin-top:-110px;">เหรัญญิก</div>
+			<div style="font-size: 18px;margin-left: 780px;margin-top:-110px;">เจ้าหน้าที่รับเงิน</div>
 			';
 
 	$dompdf = new Dompdf([
