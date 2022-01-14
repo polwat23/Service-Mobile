24bd21e3276825f92435ed69f2ca635f26ab3f25
dev_gensoft <developer@gensoft.co.th>
2022-01-14 15:40:42 +0700

    edit webshow_flag dividend

--- external_service/backupDB.bat
+++ external_service/backupDB.bat
@@ -1,24 +1,30 @@
 @echo off
 
+for /f "skip=1" %%x in ('wmic os get localdatetime') do if not defined MyDate set MyDate=%%x
+for /f %%x in ('wmic path win32_localtime get /format:list ^| findstr "="') do set %%x
+set fmonth=00%Month%
+set fday=00%Day%
+set today=%Year%%fmonth:~-2%%fday:~-2%
 
-del C:\Mobile\service-stk\external_service\backupDB_stk.zip
+set DAY_OF_LIFE=1
+set DBHOST=localhost
+set DBPORT=3306
+set DBUSER=root
+set DBPASS=@STK2021
+set DBNAME=mobile_stk
+set PROJECT_PATH=C:\Mobile\service-stk\
+set BACKUP_PATH=%PROJECT_PATH%backup\
 
-cd C:\Program Files\MariaDB 10.4\bin
+IF NOT EXIST %BACKUP_PATH% mkdir %BACKUP_PATH%
 
-C:
+"C:\Program Files\MariaDB 10.5\bin\mysqldump.exe" -h%DBHOST% -P%DBPORT% -u %DBUSER% -p%DBPASS% %DBNAME% > %BACKUP_PATH%%DBNAME%.sql
 
-mysqldump.exe -hlocalhost -P3306 -u root -p@STK2021 mobile_stk > C:\Mobile\service-stk\external_service\backupDB_stk.sql
+"C:\Program Files\7-Zip\7z.exe" a -r %BACKUP_PATH%%today%_db.zip %BACKUP_PATH%%DBNAME%.sql
 
-"C:\Program Files\7-Zip\7z.exe" a -r C:\Mobile\service-stk\external_service\backupDB_stk.zip C:\Mobile\service-stk\external_service\backupDB_stk.sql
+del %BACKUP_PATH%%DBNAME%.sql
 
-del C:\Mobile\service-stk\external_service\backupDB_stk.sql
+"C:\Program Files\7-Zip\7z.exe" a -r %BACKUP_PATH%%today%_resource.zip %PROJECT_PATH%resource\alias_account_dept %PROJECT_PATH%resource\announce %PROJECT_PATH%resource\avatar %PROJECT_PATH%resource\gallery %PROJECT_PATH%resource\news
 
-cd C:\Program Files (x86)\WinSCP
+forfiles /P %BACKUP_PATH% /S /M *.* /D -%DAY_OF_LIFE% /C "cmd /c del @path"
 
-C:
-
-winscp.exe /command "open ftp://ftp_backup:@Gensoft2018@203.154.140.14/incoming" "put C:\Mobile\service-stk\external_service\backupDB_stk.zip" "exit"
-
-del C:\Mobile\service-stk\external_service\backupDB_stk.zip
-
-pause
+::@pause
