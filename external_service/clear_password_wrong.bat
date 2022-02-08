1ac071ab4d17dcb14b43731d9a4d2d1e3b0b7e6a
dev_gensoft <developer@gensoft.co.th>
2022-02-08 16:52:37 +0700

    edit share and notify

--- external_service/clear_password_wrong.bat
+++ external_service/clear_password_wrong.bat
@@ -1,3 +1,3 @@
 @echo off
 
-C:\Mobile\php7.4\php.exe C:\Mobile\service-nkh\external_service\clear_password_wrong.php
+C:\Mobile\php7.4\php.exe C:\Mobile\service-smnk\external_service\clear_password_wrong.php
--- external_service/notify_loan.bat
+++ external_service/notify_loan.bat
@@ -1,3 +1,3 @@
 @echo off
 
-C:\Mobile\php7.4\php.exe C:\Mobile\service-nkh\external_service\notification_loan_trigger.php
+C:\Mobile\php7.4\php.exe C:\Mobile\service-smnk\external_service\notification_loan_trigger.php
