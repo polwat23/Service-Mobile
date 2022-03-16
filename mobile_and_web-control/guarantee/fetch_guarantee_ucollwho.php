e2b6f49f35c28ffc9744611077280529d179a561
dev_gensoft <developer@gensoft.co.th>
2022-03-16 15:12:51 +0700

    edit guarantee show loanbalance

--- mobile_and_web-control/guarantee/fetch_guarantee_ucollwho.php
+++ mobile_and_web-control/guarantee/fetch_guarantee_ucollwho.php
@@ -11,7 +11,8 @@ if($lib->checkCompleteArgument(['menu_component'],$dataComing)){
 											LNTYPE.loantype_desc as TYPE_DESC,
 											PRE.PRENAME_DESC,MEMB.MEMB_NAME,MEMB.MEMB_SURNAME,
 											LCM.MEMBER_NO AS MEMBER_NO,
-											NVL(LCM.LOANAPPROVE_AMT,0) as LOANAPPROVE_AMT
+											NVL(LCM.LOANAPPROVE_AMT,0) as LOANAPPROVE_AMT,
+											LCM.principal_balance as LOAN_BALANCE
 											FROM
 											LNCONTCOLL LCC LEFT JOIN LNCONTMASTER LCM ON  LCC.LOANCONTRACT_NO = LCM.LOANCONTRACT_NO
 											LEFT JOIN MBMEMBMASTER MEMB ON LCM.MEMBER_NO = MEMB.MEMBER_NO
@@ -26,6 +27,7 @@ if($lib->checkCompleteArgument(['menu_component'],$dataComing)){
 		while($rowUcollwho = $getUcollwho->fetch(PDO::FETCH_ASSOC)){
 			$arrayColl = array();
 			$arrayColl["CONTRACT_NO"] = $rowUcollwho["LOANCONTRACT_NO"];
+			$arrayColl["LOAN_BALANCE"] = number_format($rowUcollwho["LOAN_BALANCE"],2);
 			$arrayColl["TYPE_DESC"] = $rowUcollwho["TYPE_DESC"];
 			$arrayColl["MEMBER_NO"] = $rowUcollwho["MEMBER_NO"];
 			$arrayAvarTar = $func->getPathpic($rowUcollwho["MEMBER_NO"]);
