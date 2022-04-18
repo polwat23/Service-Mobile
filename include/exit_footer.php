bd9c29397d372d88dd95a6bfece721a08862193e
dev_gensoft <developer@gensoft.co.th>
2022-04-18 10:35:26 +0700

    add sign response

--- include/exit_footer.php
+++ include/exit_footer.php
@@ -1,6 +1,12 @@
 <?php
+$response = json_encode($arrayResult);
+if ($forceNewSecurity == true) {
+	$signature = "";
+	openssl_sign($response, $signature, $gensoftSCPrivatekey, OPENSSL_ALGO_SHA512);
+	header("Response_token: ".base64_encode($signature));
+}
 ob_flush();
-echo json_encode($arrayResult);
+echo $response;
 ob_end_clean();
 exit();
 ?>
