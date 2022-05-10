172b83f6ea29a1b6d3639eeb79f829fbbb950387
dev_gensoft <developer@gensoft.co.th>
2022-05-10 17:05:31 +0700

    edit loanrequest and autoload

--- mobile_and_web-control/credit/fetch_loan_credit.php
+++ mobile_and_web-control/credit/fetch_loan_credit.php
@@ -5,55 +5,169 @@ if($lib->checkCompleteArgument(['menu_component'],$dataComing)){
 	if($func->check_permission($payload["user_type"],$dataComing["menu_component"],'LoanCredit')){
 		$member_no = $configAS[$payload["member_no"]] ?? $payload["member_no"];
 		$arrGroupCredit = array();
-		$arrCanCal = array();
-		$arrCanReq = array();
-		$fetchLoanCanCal = $conmysql->prepare("SELECT loantype_code,is_loanrequest FROM gcconstanttypeloan WHERE is_creditloan = '1' ORDER BY loantype_code ASC");
-		$fetchLoanCanCal->execute();
-		while($rowCanCal = $fetchLoanCanCal->fetch(PDO::FETCH_ASSOC)){
-			$fetchLoanType = $conoracle->prepare("SELECT LOANTYPE_DESC FROM lnloantype WHERE loantype_code = :loantype_code");
-			$fetchLoanType->execute([
-				':loantype_code' => $rowCanCal["loantype_code"]
-			]);
-			$rowLoanType = $fetchLoanType->fetch(PDO::FETCH_ASSOC);
-			if(isset($rowLoanType["LOANTYPE_DESC"]) && $rowLoanType["LOANTYPE_DESC"] != ""){
+		$arrayLoantype = array();
+		$getLoantypeCredit = $conmysql->prepare("SELECT loantype_code FROM gcconstanttypeloan WHERE is_creditloan = '1'");
+		$getLoantypeCredit->execute();
+		while($rowLoanType = $getLoantypeCredit->fetch(PDO::FETCH_ASSOC)){
+			$arrayLoantype[] = "'".$rowLoanType["loantype_code"]."'";
+		}
+		if(isset($dataComing["salary"]) && $dataComing["salary"] > 0){
+			$fetchCredit = $conoracle->prepare("SELECT lt.loantype_desc AS LOANTYPE_DESC,lc.maxloan_amt as MAXLOAN_AMT,LT.loantype_code as LOANTYPE_CODE,
+												(sm.sharestk_amt*sh.unitshare_value*lc.multiple_share ) + (NVL(".$dataComing["salary"].",15000)*lc.multiple_salary ) AS CREDIT_AMT
+												FROM lnloantypecustom lc LEFT JOIN lnloantype lt ON lc.loantype_code = lt.loantype_code,
+												shsharemaster sm LEFT JOIN mbmembmaster mb ON sm.member_no = mb.member_no,shsharetype sh
+												WHERE mb.member_no = :member_no AND sm.SHAREMASTER_STATUS = '1' AND LT.LOANGROUP_CODE IN ( '01','02' )
+												AND LT.LOANTYPE_CODE IN (".implode(",",$arrayLoantype).")
+												AND TRUNC(MONTHS_BETWEEN (SYSDATE,mb.member_date ) /12 *12) BETWEEN lc.startmember_time and lc.endmember_time
+												AND sm.sharestk_amt*sh.unitshare_value BETWEEN lc.startshare_amt AND lc.endshare_amt
+												AND NVL(".$dataComing["salary"].",15000) BETWEEN lc.startsalary_amt AND lc.endsalary_amt
+												GROUP BY LT.loantype_code,lt.loantype_desc,lc.maxloan_amt,(sm.sharestk_amt*sh.unitshare_value*lc.multiple_share ) + (NVL(".$dataComing["salary"].",15000)*lc.multiple_salary)");
+			$fetchCredit->execute([':member_no' => $member_no]);
+			while($rowCredit = $fetchCredit->fetch(PDO::FETCH_ASSOC)){
 				$arrCredit = array();
-				$maxloan_amt = 0;
-				$arrCollShould = array();
-				$arrOtherInfo = array();
-				$arrOldContract = array();
-				$canRequest = FALSE;
-				if(file_exists('calculate_loan_'.$rowCanCal["loantype_code"].'.php')){
-					include('calculate_loan_'.$rowCanCal["loantype_code"].'.php');
+				if($rowCredit["CREDIT_AMT"] > $rowCredit["MAXLOAN_AMT"]){
+					$loan_amt = $rowCredit["MAXLOAN_AMT"];
 				}else{
-					include('calculate_loan_etc.php');
+					$loan_amt = $rowCredit["CREDIT_AMT"];
 				}
-				if($canRequest === TRUE){
-					$canRequest = $rowCanCal["is_loanrequest"] == '1' ? TRUE : FALSE;
-					$CheckIsReq = $conmysql->prepare("SELECT reqloan_doc,req_status
-																FROM gcreqloan WHERE loantype_code = :loantype_code and member_no = :member_no and req_status NOT IN('-9','9','1')");
-					$CheckIsReq->execute([
-						':loantype_code' => $rowCanCal["loantype_code"],
-						':member_no' => $member_no
-					]);
-					if($CheckIsReq->rowCount() > 0 || $maxloan_amt <= 0){
-						$canRequest = FALSE;
-					}else {
-						if(!$func->check_permission($payload["user_type"],'LoanRequestForm','LoanRequestForm')){
-							$canRequest = FALSE;
+				$getMaxPeriod = $conoracle->prepare("SELECT MAX_PERIOD FROM LNLOANTYPEPERIOD WHERE LOANTYPE_CODE = ? and money_from <= ? and money_to >= ?");
+				$getMaxPeriod->execute([$rowCredit["LOANTYPE_CODE"],$loan_amt,$loan_amt]);
+				$rowMaxPeriod = $getMaxPeriod->fetch(PDO::FETCH_ASSOC);
+				$arrCredit["MAX_PERIOD"] = $rowMaxPeriod["MAX_PERIOD"];
+				$getIntRate = $conoracle->prepare("SELECT LIR.INTEREST_RATE AS INTEREST_RATE
+																	FROM LNLOANTYPE LP LEFT JOIN LNCFLOANINTRATEDET LIR
+																	ON LP.INTTABRATE_CODE = LIR.LOANINTRATE_CODE WHERE SYSDATE 
+																	BETWEEN TO_CHAR(LIR.EFFECTIVE_DATE, 'YYYYMMDD') AND TO_CHAR(LIR.EXPIRE_DATE, 'YYYYMMDD') AND LP.LOANTYPE_CODE = :loantype_code");
+				$getIntRate->execute([':loantype_code' => $rowCredit["LOANTYPE_CODE"]]);
+				$rowIntRate = $getIntRate->fetch(PDO::FETCH_ASSOC);
+				$int_rate = $rowIntRate["INTEREST_RATE"] / 100;
+				if($loan_amt % 1000 > 0){
+					$loan_amt = floor($loan_amt - ($loan_amt % 1000));
+				}
+				$intCal = (($loan_amt * $int_rate) * 31) / 365;
+				$interest = $lib->roundDecimal($intCal,1);
+				$actualCredit = $loan_amt;
+				$pay_period = $loan_amt / $rowMaxPeriod["MAX_PERIOD"];
+				if($pay_period % 100 > 0){
+					$pay_period = floor($pay_period + (100 - ($pay_period % 100)));
+				}
+				$intCal = (($loan_amt * $int_rate) * 31) / 365;
+				$remainSalary = $dataComing["salary"] - $pay_period - $interest - $dataComing["other_exp"];
+				if($remainSalary < 0){
+					$remainSalary = 0;
+				}
+				$getSalaryBal = $conoracle->prepare("SELECT  LCB.SALARYBAL_PERCENT
+																	FROM LNLOANTYPE LN
+																	LEFT OUTER JOIN LNCFSALARYBALANCE LCB ON LN.SALARYBAL_CODE = LCB.SALARYBAL_CODE
+																	WHERE LN.LOANTYPE_CODE = :loantype_code");
+				$getSalaryBal->execute([':loantype_code' => $rowCredit["LOANTYPE_CODE"]]);
+				$rowSalaryPercent = $getSalaryBal->fetch(PDO::FETCH_ASSOC);
+				if(isset($rowSalaryPercent["SALARYBAL_PERCENT"]) && $rowSalaryPercent["SALARYBAL_PERCENT"] != ""){
+					$salaryActual = $dataComing["salary"] * ($rowSalaryPercent["SALARYBAL_PERCENT"] / 100);
+					if($remainSalary < $salaryActual){
+						$salaryCanLoan = $dataComing["salary"] -  $dataComing["other_exp"] - $salaryActual;
+						if($loan_amt  % 1000 > 0){
+							$actualCredit = floor($loan_amt - ($loan_amt % 1000));
 						}
+						$salaryCanLoan -= (($actualCredit * $int_rate) * 31) / 365;
+						if($salaryCanLoan < 0){
+							$salaryCanLoan = 0;
+						}
+						$loan_amt = $salaryCanLoan * $rowMaxPeriod["MAX_PERIOD"];
+						if($loan_amt % 1000 > 0){
+							$loan_amt = floor($loan_amt - ($loan_amt % 1000));
+						}
+						$intCal = (($loan_amt * $int_rate) * 31) / 365;
+						$interest = $lib->roundDecimal($intCal,1);
+						$pay_period = $loan_amt / $rowMaxPeriod["MAX_PERIOD"];
+						if($pay_period % 100 > 0){
+							$pay_period = floor($pay_period + (100 - ($pay_period % 100)));
+						}
+						$remainSalary = $dataComing["salary"] - $pay_period - $interest - $dataComing["other_exp"];
 					}
+				
 				}
 				
-				$arrCredit["OTHER_INFO"] = $arrOtherInfo;
-				$arrCredit["COLL_SHOULD_CHECK"] = $arrCollShould;
-				$arrCredit["OLD_CONTRACT"] = $arrOldContract;
-				$arrCredit["ALLOW_REQUEST"] = $canRequest;
-				$arrCredit["LOANTYPE_CODE"] = $rowCanCal["loantype_code"];
-				$arrCredit["LOANTYPE_DESC"] = $rowLoanType["LOANTYPE_DESC"];
-				$arrCredit["LOAN_PERMIT_AMT"] = floor($maxloan_amt - ($maxloan_amt % 100));
+				$arrOther = array();
+				$arrOther[0]["LABEL"] = "ชำระต้นต่องวด";
+				$arrOther[0]["VALUE"] = number_format($pay_period,2);
+				$arrOther[1]["LABEL"] = "ประมาณการดอกเบี้ย";
+				$arrOther[1]["VALUE"] = number_format($interest,2);
+				$arrOther[2]["LABEL"] = "เงินเดือนคงเหลือ";
+				$arrOther[2]["VALUE"] = number_format($remainSalary,2);
+				$arrOther[2]["VALUE_TEXT_PROPS"] = ["color" => "red"];
+				$arrCredit["FLAG_SHOW_RECV_NET"] = FALSE;
+				$arrCredit["LOANTYPE_DESC"] = $rowCredit["LOANTYPE_DESC"];
+				$arrCredit["LOANTYPE_CODE"] = $rowCredit["LOANTYPE_CODE"];
+				$arrCredit['LOAN_PERMIT_AMT'] = $loan_amt ?? 0;
+				$arrCredit['MAXLOAN_AMT'] = $loan_amt ?? 0;
+				$arrCredit['LOAN_RECEIVE_NET'] = $loan_amt - $interest ?? 0;
+				$arrCredit["OLD_CONTRACT"] = [];
+				$arrCredit["OTHER_INFO"] = $arrOther;
 				$arrGroupCredit[] = $arrCredit;
 			}
+		}else{
+			$fetchCredit = $conoracle->prepare("SELECT lt.loantype_desc AS LOANTYPE_DESC,lc.maxloan_amt as MAXLOAN_AMT,LT.loantype_code as LOANTYPE_CODE,
+												(sm.sharestk_amt*sh.unitshare_value*lc.multiple_share ) + (NVL(mb.salary_amount,15000)*lc.multiple_salary ) AS CREDIT_AMT
+												FROM lnloantypecustom lc LEFT JOIN lnloantype lt ON lc.loantype_code = lt.loantype_code,
+												shsharemaster sm LEFT JOIN mbmembmaster mb ON sm.member_no = mb.member_no,shsharetype sh
+												WHERE mb.member_no = :member_no AND sm.SHAREMASTER_STATUS = '1' AND LT.LOANGROUP_CODE IN ( '01','02' )
+												AND LT.LOANTYPE_CODE IN (".implode(",",$arrayLoantype).")
+												AND TRUNC(MONTHS_BETWEEN (SYSDATE,mb.member_date ) /12 *12) BETWEEN lc.startmember_time and lc.endmember_time
+												AND sm.sharestk_amt*sh.unitshare_value BETWEEN lc.startshare_amt AND lc.endshare_amt
+												AND NVL(mb.salary_amount,15000) BETWEEN lc.startsalary_amt AND lc.endsalary_amt
+												GROUP BY LT.loantype_code,lt.loantype_desc,lc.maxloan_amt,(sm.sharestk_amt*sh.unitshare_value*lc.multiple_share ) + (NVL(mb.salary_amount,15000)*lc.multiple_salary)");
+			$fetchCredit->execute([':member_no' => $member_no]);
+			while($rowCredit = $fetchCredit->fetch(PDO::FETCH_ASSOC)){
+				$arrCredit = array();
+				if($rowCredit["CREDIT_AMT"] > $rowCredit["MAXLOAN_AMT"]){
+					$loan_amt = $rowCredit["MAXLOAN_AMT"];
+				}else{
+					$loan_amt = $rowCredit["CREDIT_AMT"];
+				}
+				if($loan_amt % 1000 > 0){
+					$loan_amt = floor($loan_amt - ($loan_amt % 1000));
+				}
+				$getMaxPeriod = $conoracle->prepare("SELECT MAX_PERIOD FROM LNLOANTYPEPERIOD WHERE LOANTYPE_CODE = ? and money_from <= ? and money_to >= ?");
+				$getMaxPeriod->execute([$rowCredit["LOANTYPE_CODE"],$loan_amt,$loan_amt]);
+				$rowMaxPeriod = $getMaxPeriod->fetch(PDO::FETCH_ASSOC);
+				$arrCredit["MAX_PERIOD"] = $rowMaxPeriod["MAX_PERIOD"];
+				$getIntRate = $conoracle->prepare("SELECT LIR.INTEREST_RATE AS INTEREST_RATE
+																	FROM LNLOANTYPE LP LEFT JOIN LNCFLOANINTRATEDET LIR
+																	ON LP.INTTABRATE_CODE = LIR.LOANINTRATE_CODE WHERE SYSDATE
+																	BETWEEN TO_CHAR(LIR.EFFECTIVE_DATE, 'YYYYMMDD') AND TO_CHAR(LIR.EXPIRE_DATE, 'YYYYMMDD') AND LP.LOANTYPE_CODE = :loantype_code");
+				$getIntRate->execute([':loantype_code' => $rowCredit["LOANTYPE_CODE"]]);
+				$rowIntRate = $getIntRate->fetch(PDO::FETCH_ASSOC);
+				$int_rate = $rowIntRate["INTEREST_RATE"] / 100;
+				$intCal = (($loan_amt * $int_rate) * 31) / 365;
+				$pay_period = $loan_amt / $rowMaxPeriod["MAX_PERIOD"];
+				$pay_period = floor($pay_period + (100 - ($pay_period % 100)));
+				$interest = $lib->roundDecimal($intCal,1);
+				$remainSalary = $pay_period;
+				$arrOther = array();
+				$arrOther[0]["LABEL"] = "ชำระต้นต่องวด";
+				$arrOther[0]["VALUE"] = number_format($pay_period,2);
+				$arrOther[1]["LABEL"] = "ประมาณการดอกเบี้ย";
+				$arrOther[1]["VALUE"] = number_format($interest,2);
+				$arrCredit["FLAG_SHOW_RECV_NET"] = FALSE;
+				$arrCredit["LOANTYPE_DESC"] = $rowCredit["LOANTYPE_DESC"];
+				$arrCredit["LOANTYPE_CODE"] = $rowCredit["LOANTYPE_CODE"];
+				$arrCredit['LOAN_PERMIT_AMT'] = $loan_amt ?? 0;
+				$arrCredit['MAXLOAN_AMT'] = $loan_amt ?? 0;
+				$arrCredit['LOAN_RECEIVE_NET'] = $loan_amt - $interest ?? 0;
+				$arrCredit["OLD_CONTRACT"] = [];
+				$arrCredit["OTHER_INFO"] = $arrOther;
+				$arrGroupCredit[] = $arrCredit;
+
+			}
 		}
+		$arrayResult["NOTE_INPUT_TITLE"] = "";
+		$arrayResult["NOTE_INPUT"] = "*** ค่าใช้จ่ายในสลิปเงินเดือนทั้งหมด และค่าหุ้นสหกรณ์ ยกเว้นเงินต้นและดอกเบี้ย";
+		$arrayResult["NOTE_INPUT_TEXT_COLOR"] = "red";
+		$arrayResult["NOTE"] = "ผลการคำนวณข้างต้นเป็นเพียงการประมาณการเท่านั้น​ การพิจารณาอนุมัติสินเชื่อเป็นไปตามหลักเกณฑ์และเงื่อนไขที่สหกรณ์ฯ​ กำหนด";
+		$arrayResult["NOTE_TEXT_COLOR"] = "red";
+		$arrayResult["INPUT_SALARY"] = TRUE;
+		$arrayResult["INPUT_OTHER_EXP"] = TRUE;
 		$arrayResult["LOAN_CREDIT"] = $arrGroupCredit;
 		$arrayResult['RESULT'] = TRUE;
 		require_once('../../include/exit_footer.php');
