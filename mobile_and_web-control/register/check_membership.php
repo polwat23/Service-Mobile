b46c73c30f84e3ee81eac7358c7168392b8d3177
dev_gensoft <developer@gensoft.co.th>
2022-06-01 11:19:34 +0700

    policy pdpa

--- mobile_and_web-control/register/check_membership.php
+++ mobile_and_web-control/register/check_membership.php
@@ -55,7 +55,14 @@ if($lib->checkCompleteArgument(['member_no','id_card','api_token','unique_id'],$
 			$arrayResult['MEMBER_NO'] = $member_no;
 			$arrayResult['CARD_PERSON'] = $dataComing["id_card"];
 			$arrayResult['MEMBER_FULLNAME'] = $rowMember["PRENAME_DESC"].$rowMember["MEMB_NAME"].' '.$rowMember["MEMB_SURNAME"];
-			$arrayResult['RESULT'] = TRUE;
+			$arrayResult['RESULT'] = TRUE;			
+			if ($forceNewSecurity == true) {
+				$newArrayResult = array();
+				$newArrayResult['ENC_TOKEN'] = $lib->generate_jwt_token($arrayResult, $jwt_token, $config["SECRET_KEY_JWT"]);
+				$arrayResult = array();
+				$arrayResult = $newArrayResult;
+			}
+
 			require_once('../../include/exit_footer.php');
 		}else{
 			$arrayResult['RESPONSE_CODE'] = "WS0003";
