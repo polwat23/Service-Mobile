b79e573f03838dd1d1f536fe78d5357cedd1c326
dev_gensoft <developer@gensoft.co.th>
2022-06-09 16:40:53 +0700

    service ryt test

---
+++ linebot/service/notify/update_notify.php
@@ -0,0 +1,22 @@
+<?php
+$data = $lineLib->getMemberNo($user_id);
+$member_no = $data;
+if($message == " เปิดการแจ้งเตือน"){
+	$is_notify = 1;
+}else{
+	$is_notify = 0;
+}
+
+$insertNotify =  $conmysql->prepare("UPDATE lbnotify SET is_notify = :is_notify ");
+	if($insertNotify->execute([
+		':is_notify' => $is_notify
+	])){
+		$messageResponse = $message." สำเร็จ";
+		$dataPrepare = $lineLib->prepareMessageText($messageResponse);
+		$arrPostData["messages"] = $dataPrepare;
+		$arrPostData["replyToken"] = $reply_token;
+	}else{
+		$message_error = "Line Bot insert ลง  lbnotify  ไม่ได้".$filename."\n".'sql => '.$insertNotify;
+		$lib->sendLineNotify($message_error);
+	}
+?>
